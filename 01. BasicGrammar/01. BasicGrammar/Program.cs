2adfd1fe12d31b8cb2c200208774dedd45cc9c42
JamesPark <woghd8503@naver.com>
2022-02-13 02:17:13 +0900

    01.BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/Program.cs
+++ 01. BasicGrammar/01. BasicGrammar/Program.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace _01.BasicGrammar
 {
-    class Program
+    class IntegralTypes
     {
         static void Main(string[] args)
         {
+            sbyte a = -10;
+            byte b = 40;
+
+            Console.WriteLine($"a={a}, b={b}");
+
+            short c = -30000;
+            ushort d = 6000;
         }
     }
 }
