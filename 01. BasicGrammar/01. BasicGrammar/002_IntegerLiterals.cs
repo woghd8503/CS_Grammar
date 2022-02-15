02ff5e00ec3cd8bacf1591635355acb37f3f52f9
James <woghd8503@naver.com>
2022-02-15 23:57:05 +0900

    01.BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/002_IntegerLiterals.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.BasicGrammar
+{
+    class IntegerLiterals
+    {
+        static void Main(string[] args)
+        {
+            byte a = 240;       // 10진수 리터럴
+            Console.WriteLine($"a={a}");
+
+            by
+        }
+    }
+}
