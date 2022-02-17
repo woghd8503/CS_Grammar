19e4a72f1f4bf245fcb3cfb614c10d1acf9921bb
JamesPark <woghd8503@naver.com>
2022-02-18 00:57:49 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/004_Overflow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.BasicGrammar
+{
+    class _Overflow
+    {
+        static void Main(string[] args)
+        {
+            uint a = uint.MaxValue;
+
+            Console.WriteLine(a);
+
+            a = a + 1;
+
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ 01. BasicGrammar/01. BasicGrammar/005_FloatingPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.BasicGrammar
+{
+    class _005_FloatingPoint
+    {
+        static void Main(string[] args)
+        {
+            float a = 3.1415_9265_3589_7932_3826f;
+            Console.WriteLine(a);
+        }
+    }
+}
