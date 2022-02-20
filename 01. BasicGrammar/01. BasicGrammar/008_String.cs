d57d3c8413ca911351c52e40cb8da2cb22c1451a
JamesPark <woghd8503@naver.com>
2022-02-20 12:11:41 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/008_String.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.BasicGrammar
+{
+    class String
+    {
+        static void Main(string[] args)
+        {
+            string a = "안녕하세요?";
+            string b = "백상현입니다.";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
