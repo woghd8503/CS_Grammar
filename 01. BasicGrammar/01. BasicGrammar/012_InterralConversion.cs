abd427501cc136e8b1db829d9efc1168e699f1c0
JamesPark <woghd8503@naver.com>
2022-02-21 00:54:20 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/012_InterralConversion.cs
+++ 01. BasicGrammar/01. BasicGrammar/012_InterralConversion.cs
@@ -17,14 +17,6 @@ namespace _01.BasicGrammar
             Console.WriteLine(a);
             Console.WriteLine(b);
             Console.WriteLine(c);
-
-            double x = 3.1414213; 
-            object y = x;           // x에 담긴 값을 박싱해서 저장
-            double z = (double)y;   // y에 담긴 값을 언박싱해서 스택에 저장
-
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
         }
     }
 }
