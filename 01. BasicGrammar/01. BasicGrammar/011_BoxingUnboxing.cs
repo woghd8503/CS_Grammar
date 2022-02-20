9dd85baf5ad706261b8d8925d37a2723c91c629c
JamesPark <woghd8503@naver.com>
2022-02-21 00:49:14 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/011_BoxingUnboxing.cs
+++ 01. BasicGrammar/01. BasicGrammar/011_BoxingUnboxing.cs
@@ -6,19 +6,17 @@ using System.Threading.Tasks;
 
 namespace _01.BasicGrammar
 {
-    class String
+    class BoxingUnboxing
     {
         static void Main(string[] args)
         {
-            object a = 123;
-            object b = 3.141592653589793238462643383279m;
-            object c = true;
-            object d = "안녕하세요.";
+            int a = 123;
+            object b = (object)a;   // a에 담긴 값을 박싱해서 힙에 저장
+            int c = (int)b;         // b에 담긴 값을 언박싱해서 스택에 저장
 
             Console.WriteLine(a);
             Console.WriteLine(b);
             Console.WriteLine(c);
-            Console.WriteLine(d);
         }
     }
 }
