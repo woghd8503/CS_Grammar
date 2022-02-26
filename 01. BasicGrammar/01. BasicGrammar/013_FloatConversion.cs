02bda53b6d87fe41a9d012d82e0a719e4ea32804
James <woghd8503@naver.com>
2022-02-26 23:43:28 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/013_FloatConversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.BasicGrammar
+{
+    class BoxingUnboxing
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            object b = (object)a;   // a에 담긴 값을 박싱해서 힙에 저장
+            int c = (int)b;         // b에 담긴 값을 언박싱해서 스택에 저장
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
