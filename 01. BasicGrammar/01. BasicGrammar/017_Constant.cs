3286f406c9b0d8ecca548c7a110dd5e7ca61aa67
James <woghd8503@naver.com>
2022-02-28 02:39:16 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/017_Constant.cs
+++ 01. BasicGrammar/01. BasicGrammar/017_Constant.cs
@@ -10,21 +10,11 @@ namespace _01.BasicGrammar
     {
         static void Main(string[] args)
         {
-            int a = 123;
-            string b = a.ToString();
-            Console.WriteLine(b);
+            const int MAX_INT = 2147483647;
+            const int MIN_INT = -2147483648;
 
-            float c = 3.14f;
-            string d = c.ToString();
-            Console.WriteLine(d);
-
-            string e = "123456";
-            int f = Convert.ToInt32(e);
-            Console.WriteLine(f);
-
-            string g = "1.2345";
-            float h = float.Parse(g);
-            Console.WriteLine(h);
+            Console.WriteLine(MAX_INT);
+            Console.WriteLine(MIN_INT);
         }
     }
 }
