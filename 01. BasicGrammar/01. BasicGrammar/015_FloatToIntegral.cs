4ed80e9e9632afe2e0e775499733be926f261efc
James <woghd8503@naver.com>
2022-02-28 02:32:54 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/015_FloatToIntegral.cs
+++ 01. BasicGrammar/01. BasicGrammar/015_FloatToIntegral.cs
@@ -6,17 +6,25 @@ using System.Threading.Tasks;
 
 namespace _01.BasicGrammar
 {
-    class FloatConversion
+    class StringNumberConversion
     {
         static void Main(string[] args)
         {
-            float a = 0.9f;
-            int b = (int)a;
+            int a = 123;
+            string b = a.ToString();
             Console.WriteLine(b);
 
-            float c = 1.1f;
-            int d = (int)c;
+            float c = 3.14f;
+            string d = c.ToString();
             Console.WriteLine(d);
+
+            string e = "123456";
+            int f = Convert.ToInt32(e);
+            Console.WriteLine(f);
+
+            string g = "1.2345";
+            float h = float.Parse(g);
+            Console.WriteLine(h);
         }
     }
 }
---
+++ 01. BasicGrammar/01. BasicGrammar/016_StringNumberConversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.BasicGrammar
+{
+    class FloatConversion
+    {
+        static void Main(string[] args)
+        {
+            float a = 0.9f;
+            int b = (int)a;
+            Console.WriteLine(b);
+
+            float c = 1.1f;
+            int d = (int)c;
+            Console.WriteLine(d);
+        }
+    }
+}
