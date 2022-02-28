58465929e8ee4610e2d51cedd7da9269e542f85c
James <woghd8503@naver.com>
2022-03-01 01:48:53 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/022_UsingVar.cs
+++ 01. BasicGrammar/01. BasicGrammar/022_UsingVar.cs
@@ -23,6 +23,19 @@ namespace _01.BasicGrammar
         {
             var a = 20;
             Console.WriteLine("Type: {0}, Value: {1}", a.GetType(), a);
+
+            var b = 3.1414231;
+            Console.WriteLine("Type: {0}, Value: {1}", b.GetType(), b);
+
+            var c = "Hello World";
+            Console.WriteLine("Type: {0}, Value: {1}", c.GetType(), c);
+
+            var d = new int[] { 10, 20, 30 };
+            Console.Write("Type: {0}, Value: ", d.GetType());
+            foreach(var e in d)
+                Console.Write("{0} ", e);
+
+            Console.WriteLine();
         }
     }
 }
