40a23ac0889b2b545c7df980610d60136e39614b
James <woghd8503@naver.com>
2022-03-02 00:03:44 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/024_StringSearch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.BasicGrammar
+{
+    class StringSearch
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Good Morning";
+        }
+    }
+}
