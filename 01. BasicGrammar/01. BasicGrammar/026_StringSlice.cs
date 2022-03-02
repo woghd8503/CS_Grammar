d566862660edafb084ffe0a1d24443018def32a4
James <woghd8503@naver.com>
2022-03-03 04:59:41 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/026_StringSlice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class StringSlice
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Good morning";
+
+            WriteLine(greeting.Substring(0, 5)); // "Good"
+        }
+    }
+}
