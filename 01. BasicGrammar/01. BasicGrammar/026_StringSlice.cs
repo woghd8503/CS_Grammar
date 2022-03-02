489886e812e8dbe59e74377ff22c40c86c44b44a
James <woghd8503@naver.com>
2022-03-03 05:07:22 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/026_StringSlice.cs
+++ 01. BasicGrammar/01. BasicGrammar/026_StringSlice.cs
@@ -14,6 +14,15 @@ namespace _01.BasicGrammar
             string greeting = "Good morning";
 
             WriteLine(greeting.Substring(0, 5)); // "Good"
+            WriteLine(greeting.Substring(5));   // "morning"
+            WriteLine();
+
+            string[] arr = greeting.Split(
+                new string[] { " " }, StringSplitOptions.None);
+            WriteLine("World Count : {0}", arr.Length);
+
+            foreach (string element in arr)
+                WriteLine("{0}", element);
         }
     }
 }
