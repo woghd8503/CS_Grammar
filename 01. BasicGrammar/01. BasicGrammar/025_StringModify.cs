9893507da7f158caf7b72792fd94b9e6ed51f70f
James <woghd8503@naver.com>
2022-03-03 04:27:43 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/025_StringModify.cs
+++ 01. BasicGrammar/01. BasicGrammar/025_StringModify.cs
@@ -15,6 +15,11 @@ namespace _01.BasicGrammar
             WriteLine("ToUpper() : '{0}'", "abc".ToUpper());
 
             WriteLine("Insert() : '{0}'", "Happy Friday!".Insert(5, " Sunny"));
+            WriteLine("Remove() : '{0}'", "I Don't Love You.".Remove(2, 6));
+
+            WriteLine("Trim() : '{0}'", " No Spaces ".Trim());
+            WriteLine("TrimStart() : '{0}'", " No Spaces ".TrimStart());
+            WriteLine("TrimEnd() : '{0}'", " No Spaces ".TrimEnd());
         }
     }
 }
