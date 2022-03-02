2e47f08f39ee0ad7a0531e96c696b9e6b2ffa825
James <woghd8503@naver.com>
2022-03-03 04:09:05 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/025_StringModify.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class StringModify
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("ToLower() : '{0}'", "ABC".ToLower());
+        }
+    }
+}
