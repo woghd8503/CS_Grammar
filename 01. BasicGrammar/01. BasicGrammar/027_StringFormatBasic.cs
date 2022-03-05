6d0e3e4f1061eb07a96f79544f964109faa4124f
James <woghd8503@naver.com>
2022-03-06 01:35:01 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/027_StringFormatBasic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class StringFormatBasic
+    {
+        static void Main(string[] args)
+        {
+            string fmt = "{0, -20}{1, -15}{2, 30}";
+
+            WriteLine(fmt, "Publisher", "Author", "Title");
+        }
+    }
+}
