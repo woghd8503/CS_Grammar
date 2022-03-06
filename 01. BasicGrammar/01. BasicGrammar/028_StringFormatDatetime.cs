80f1c8fd8558a0913f85cab646336d22ed46e8d4
James park <woghd8503@naver.com>
2022-03-06 20:18:27 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/028_StringFormatDatetime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class StringFormatDatetime
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2018, 11, 3, 23, 18, 22);
+
+            WriteLine("12시간 형식: {0:yyyy-MM-DD tt hh:mm:ss (ddd)}", dt);
+            WriteLine("24시간 형식: {0:yyyy-MM-DD HH:mm:ss (dddd)}", dt);
+
+            CultureInfo ciko = new CultureInfo("ko-KR");
+            WriteLine();
+            WriteLine(dt.ToString("yyyy-MM-dd tt hh:mm:ss (ddd)", ciko));
+
+        }
+    }
+}
