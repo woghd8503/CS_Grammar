42d2e326a8e717d2663ef64f15a947f7adc6c603
James <woghd8503@naver.com>
2022-03-06 23:58:10 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/027_StringFormatBasic.cs
+++ 01. BasicGrammar/01. BasicGrammar/027_StringFormatBasic.cs
@@ -14,6 +14,9 @@ namespace _01.BasicGrammar
             string fmt = "{0, -20}{1, -15}{2, 30}";
 
             WriteLine(fmt, "Publisher", "Author", "Title");
+            WriteLine(fmt, "Marvel", "Stan Lee", "Iron Man");
+            WriteLine(fmt, "Hanbit", "Sanghyun Park", "This is C#");
+            WriteLine(fmt, "Prentice Hall", "K&R", "The C Programming Language");
         }
     }
 }
