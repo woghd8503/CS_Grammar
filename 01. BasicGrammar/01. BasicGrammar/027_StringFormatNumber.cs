38481b1e3290b7ea1104f63d877e18b5cd6a9e4c
James park <woghd8503@naver.com>
2022-03-06 19:46:14 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/027_StringFormatNumber.cs
+++ 01. BasicGrammar/01. BasicGrammar/027_StringFormatNumber.cs
@@ -17,10 +17,19 @@ namespace _01.BasicGrammar
 
             // X : 16진수
             WriteLine("16진수: 0x{0:X}", 0xFF1234);
-            WriteLine("16진수: 0x{0X8}", 0xFF1234);
+            WriteLine("16진수: 0x{0:X8}", 0xFF1234);
 
+            // N : 숫자
+            WriteLine("숫자: {0:N}", 123456789);
+            WriteLine("숫자: {0:N0}", 123456789); // 자릿수 0은 소수점 이하를 제거함.
+
+            // F : 고정소수점
+            WriteLine("고정소수점: {0:F}", 123.45);
+            WriteLine("고정소수점: {0:F5}", 123.456);
+
+            // E : 공학용
+            WriteLine("공학: {0:E}", 123.456789);
 
         }
-        
     }
 }
