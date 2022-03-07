aea06a9828fc9f0279be562d4fd11d358c327190
James <woghd8503@naver.com>
2022-03-08 00:58:58 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/034_StringConcatenate.cs
+++ 01. BasicGrammar/01. BasicGrammar/034_StringConcatenate.cs
@@ -8,15 +8,11 @@ using static System.Console;
 
 namespace _01.BasicGrammar
 {
-    class StringConcatenate
+    class RelationalOperator
     {
         static void Main(string[] args)
         {
-            string result = "123" + "456";
-            Console.WriteLine(result);
 
-            result = "Hello" + " " + "World!";
-            Console.WriteLine(result);
         }
     }
 }
