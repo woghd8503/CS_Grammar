b709c929f0e3a06ff90dee8c8709565221a71235
James <woghd8503@naver.com>
2022-03-08 00:53:40 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/033_InDecOperator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class ArithmaticOperators
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a++);
+            Console.WriteLine(++a);
+
+            Console.WriteLine(a--);
+            Console.WriteLine(--a);
+        }
+    }
+}
