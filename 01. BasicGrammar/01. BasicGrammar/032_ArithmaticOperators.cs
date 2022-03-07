ae77dd3645685a73abb4946b0e672a864ed61c3c
James <woghd8503@naver.com>
2022-03-08 00:49:28 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/032_ArithmaticOperators.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class ArithmaticOperators
+    {
+        static void Main(string[] args)
+        {
+            int a = 111 + 222;
+            Console.WriteLine($"a : {a}");
+
+            int b = a - 100;
+            Console.WriteLine($"b : {b}");
+
+            int c = b * 10;
+            Console.WriteLine($"c : {c}");
+
+            double d = c / 6.3;
+            Console.WriteLine($"d : {d}");
+
+            Console.WriteLine($"22 / 7 = {22 / 7}({22 % 7})");
+        }
+    }
+}
