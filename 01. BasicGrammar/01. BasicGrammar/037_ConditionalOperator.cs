7baa0054c512b7590418e29aea67e8b2bc5f578a
James <woghd8503@naver.com>
2022-03-11 03:31:06 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/037_ConditionalOperator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class ConditionalOperator
+    {
+        static void Main(string[] args)
+        {
+            string result = (10 % 2) == 0 ? "짝수" : "홀수";
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 01. BasicGrammar/01. BasicGrammar/038_NullConditionalOperator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class ConditionalOperator
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a = null;
+            a?.Add("야구");
+        }
+    }
+}
