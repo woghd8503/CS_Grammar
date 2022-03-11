c273d528984891fa58c747fdb26a6d27604f8694
James <woghd8503@naver.com>
2022-03-11 14:26:21 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/038_NullConditionalOperator.cs
+++ 01. BasicGrammar/01. BasicGrammar/038_NullConditionalOperator.cs
@@ -16,7 +16,16 @@ namespace _01.BasicGrammar
             ArrayList a = null;
             a?.Add("야구"); // a?.가 null을 반환하므로 Add() 메소드는 호출되지 않음
             a?.Add("축구");
-            WriteLine()
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
+
+            a = new ArrayList();  // a는 이제 더 이상 null이 아닙니다.
+            a?.Add("야구");
+            a?.Add("축구");
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
         }
     }
 }
