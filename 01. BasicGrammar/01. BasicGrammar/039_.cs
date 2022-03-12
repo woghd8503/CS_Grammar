379a28caf8ae25476e32b7a77bf7be481511c7ce
James <woghd8503@naver.com>
2022-03-12 18:10:46 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/039_.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class NullConditionalOperator
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a = null;
+            a?.Add("야구"); // a?.가 null을 반환하므로 Add() 메소드는 호출되지 않음
+            a?.Add("축구");
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
+
+            a = new ArrayList();  // a는 이제 더 이상 null이 아닙니다.
+            a?.Add("야구");
+            a?.Add("축구");
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
+        }
+    }
+}
---
+++ 01. BasicGrammar/01. BasicGrammar/039_ShiftOperator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class ShiftOperator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing <<...");
+
+            int a = 1;
+            Console.WriteLine("a      : {0:D5} (0x{0:X8})", a);
+            Console.WriteLine("a << 1 : {0:D5} (0x{0:X8})", a << 1);
+        }
+    }
+}
