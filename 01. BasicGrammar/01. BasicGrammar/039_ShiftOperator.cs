35f15ae923ce1c638682bd797b1f9a8d97b640fe
James <woghd8503@naver.com>
2022-03-12 18:11:21 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/039_ShiftOperator.cs
+++ 01. BasicGrammar/01. BasicGrammar/039_ShiftOperator.cs
@@ -18,6 +18,8 @@ namespace _01.BasicGrammar
             int a = 1;
             Console.WriteLine("a      : {0:D5} (0x{0:X8})", a);
             Console.WriteLine("a << 1 : {0:D5} (0x{0:X8})", a << 1);
+            Console.WriteLine("a << 2 : {0:D5} (0x{0:X8})", a << 2);
+            Console.WriteLine("a << 5 : {0:D5} (0x{0:X8})", a << 5);
         }
     }
 }
