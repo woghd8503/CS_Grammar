fe466e22258d5ec9a3923eecb7de9f5e377aceca
James <woghd8503@naver.com>
2022-03-14 23:52:51 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/040_BitwiseOperator.cs
+++ 01. BasicGrammar/01. BasicGrammar/040_BitwiseOperator.cs
@@ -13,6 +13,12 @@ namespace _01.BasicGrammar
     {
         static void Main(string[] args)
         {
+            int a = 9;
+            int b = 10;
+
+            Console.WriteLine($"{a} & {b} : {a & b}");
+            Console.WriteLine($"{a} | {b} : {a | b}");
+            Console.WriteLine($"{a} ^ {b} : {a ^ b}");
 
         }
     }
