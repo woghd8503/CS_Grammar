115ae241afbf2d52a7a115e4992d781e062af9bb
James <woghd8503@naver.com>
2022-03-16 23:55:14 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/043_Test2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class Test2
+    {
+        static void Main(string[] args)
+        {
+            int? num = null;
+            Console.WriteLine($"{num ?? 0}");
+
+            num = 99;
+            Console.WriteLine($"{num ?? 0}");
+
+            string str = null;
+            Console.WriteLine($"{str ?? "Default"}");
+
+            str = "Specific";
+            Console.WriteLine($"{str ?? "Default"}");
+        }
+    }
+}
