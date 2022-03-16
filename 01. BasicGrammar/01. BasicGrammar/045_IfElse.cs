1132c2cbec613bcb0607f55e863a2f59f78d8743
James <woghd8503@naver.com>
2022-03-17 03:32:20 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/045_IfElse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class Test2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("숫자를 입력하세요. : ");
+
+            string input = Console.ReadLine();
+            int number = Int32.Parse(input);
+
+            if (number < 0)
+                Console.WriteLine("음수");
+            else if (number > 0)
+                Console.WriteLine("양수");
+            else
+                Console.WriteLine("0");
+
+            if (number % 2 == 0)
+                Console.WriteLine("짝수");
+            else
+                Console.WriteLine("홀수");
+        }
+    }
+}
