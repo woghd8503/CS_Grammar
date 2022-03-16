893b1cef390ec86272c7b0a8e7584c8ec95f9e30
James <woghd8503@naver.com>
2022-03-16 23:51:09 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/042_.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class NullCoalescing
+    {
+        static void Main(string[] args)
+        {
+            int? num = null;
+            Console.WriteLine($"{num ?? 0}");
+
+            num = 99;
+            Console.WriteLine($"{num ?? 0}");
+
+            string str = null;
+            Console.WriteLine($"{str ?? "Default"}");
+        }
+    }
+}
