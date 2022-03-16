5fc0d03fcaaefa94e8a62796a710589c8878cfa0
James <woghd8503@naver.com>
2022-03-17 03:33:57 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/046_IfIf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class Test2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("숫자를 입력하세요. : ");
+        }
+    }
+}
