594040c2830ec1b35fae8813acef5ddd971764ac
James <woghd8503@naver.com>
2022-03-17 00:04:14 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/043_Test2.cs
+++ 01. BasicGrammar/01. BasicGrammar/043_Test2.cs
@@ -13,17 +13,26 @@ namespace _01.BasicGrammar
     {
         static void Main(string[] args)
         {
-            int? num = null;
-            Console.WriteLine($"{num ?? 0}");
-
-            num = 99;
-            Console.WriteLine($"{num ?? 0}");
-
-            string str = null;
-            Console.WriteLine($"{str ?? "Default"}");
-
-            str = "Specific";
-            Console.WriteLine($"{str ?? "Default"}");
+            
         }
+        // 1. i++와 ++i의 차이점은 무엇인가?
+
+
+        // 2. 다음 보기 중에서 그 결과가 다른 것을 찾으세요
+        //  a. i = i + 1;
+        //  b. i++;
+        //  c. ++i;
+        //  d. i += 1;
+
+        // 3. 다음 코드에서 a와 b는 각각 얼마일까요?
+        // int a = 8 >> 1;
+        // int b = a >> 2;
+
+        // 4. 다음 코드에서 a는 얼마일까요?
+        // int a = 0xF0 | 0x0F;
+
+        // 5. 다음 코드에서 b는 어떤 값을 가질까요?
+        // int a = 10;
+        // string b = a == 0 ? "가나다" : "ABC";
     }
 }
