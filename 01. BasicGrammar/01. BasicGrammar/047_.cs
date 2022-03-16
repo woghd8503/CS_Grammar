193db4c0d79a5cbdf5a0502a6f4a87ed4f334cab
James <woghd8503@naver.com>
2022-03-17 03:39:05 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/047_.cs
+++ 01. BasicGrammar/01. BasicGrammar/047_.cs
@@ -9,26 +9,26 @@ using static System.Console;
 
 namespace _01.BasicGrammar
 {
-    class IfIf
+    class Test2
     {
         static void Main(string[] args)
         {
             Console.Write("숫자를 입력하세요. : ");
 
             string input = Console.ReadLine();
-            int number = Int32.Parse(input);
+            int number = Convert.ToInt32(input);
 
-            if (number < 0)
-                Console.WriteLine("음수");
-            else if (number > 0)
-                Console.WriteLine("양수");
+            if ( number > 0 )
+            {
+                if ( number % 2 == 0 )
+                    Console.WriteLine("0보다 큰 짝수.");
+                else
+                    Console.WriteLine("0보다 큰 홀수.");
+            }
             else
-                Console.WriteLine("0");
-
-            if (number % 2 == 0)
-                Console.WriteLine("짝수");
-            else
-                Console.WriteLine("홀수");
+            {
+                Console.WriteLine("0보다 작거나 같은 수.");
+            }
         }
     }
 }
