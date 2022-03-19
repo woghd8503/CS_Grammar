4fe0ef47af59f5ad51f6cb9628c06175910be942
James <woghd8503@naver.com>
2022-03-20 03:43:52 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/060_Test3.cs
+++ 01. BasicGrammar/01. BasicGrammar/060_Test3.cs
@@ -56,6 +56,30 @@ namespace _01.BasicGrammar
                 Console.WriteLine();
                 o++;
             } while (o < 5);
+
+
+
+            int num = -1;
+
+            while(num <= 0)
+            {
+                Console.Write("반복 횟수를 입력하세요");
+                String input = Console.ReadLine();
+                num = Convert.ToInt32(input);
+
+                for(int i = 0; i < num; i++)
+                {
+                    for(int j = 0; j < i + 1; j++)
+                        Console.Write("*");
+
+                    Console.WriteLine();
+                }
+
+                if(num <= 0)
+                    Console.WriteLine("0보다 작거나 같은 수는 입력할 수 없습니다.");
+            }
+
+
         }
     }
 }
