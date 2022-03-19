4a78fcc71d6d5ae26be428b292d7baea12ad62ab
James <woghd8503@naver.com>
2022-03-20 03:03:36 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/059_Goto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class Goto
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("종료 조건(숫자)을 입력하세요 :");
+
+            String input = Console.ReadLine();
+
+            int input_number = Convert.ToInt32(input);
+            int exit_number = 0;
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        if (exit_number++ == input_number)
+                            goto EXIT_FOR;
+
+                        Console.WriteLine(exit_number);
+                    }
+                }
+            }
+
+            goto EXIT_PROGRAM;
+
+        EXIT_FOR:
+            Console.WriteLine("/nExit nested for...");
+        EXIT_PROGRAM:
+            Console.WriteLine("Exit program...");
+        }
+    }
+}
