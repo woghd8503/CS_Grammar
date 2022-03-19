e536b095fa07b867de04e8aff43e0c8c34dfdba5
James <woghd8503@naver.com>
2022-03-19 23:20:45 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/050_While.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class While
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            
+            while (i > 0)
+            {
+                Console.WriteLine($"i : {i--}");
+            }
+        }
+    }
+}
