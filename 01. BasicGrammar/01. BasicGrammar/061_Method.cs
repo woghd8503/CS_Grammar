2708c26ac745ad46e81186fff0103db9bc0b3bbf
James <woghd8503@naver.com>
2022-03-20 03:49:58 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/061_Method.cs
+++ 01. BasicGrammar/01. BasicGrammar/061_Method.cs
@@ -9,6 +9,9 @@ using static System.Console;
 
 // 매개변수 Parameter와 인수 Argument
 // 매개변수는 매소드가 호출자에게서 전달받은 값을 받는 변수를 말하고, 인수는 호출자가 매개변수에 넘기는
+// 값을 뜻합니다. 예를 들어, Calculator 클래스 안에 선언한 메소드 public static int Plus( int a, int b )에서 a,
+// b가 매개변수, Calculator.Plus( 3, 4 )에서 3, 4가 인수입니다.
+
 namespace _01.BasicGrammar
 {
     class Method
