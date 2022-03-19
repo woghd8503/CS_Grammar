34a3ec458b6b51a5cc91dce8b917cd7c1125ad15
James <woghd8503@naver.com>
2022-03-19 23:31:15 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/053_ForFor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class For
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
