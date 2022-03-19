8261352f20216e2ff6d51359b823743379435c21
James <woghd8503@naver.com>
2022-03-19 23:40:48 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/057_Break.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class InfiniteFor
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (true)
+                Console.WriteLine(i++);
+        }
+    }
+}
