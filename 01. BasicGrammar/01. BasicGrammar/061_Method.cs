b2a2b0a7c44465b284d4b6c1ea77ae2d66187df5
James <woghd8503@naver.com>
2022-03-20 03:57:50 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/061_Method.cs
+++ 01. BasicGrammar/01. BasicGrammar/061_Method.cs
@@ -14,11 +14,28 @@ using static System.Console;
 
 namespace _01.BasicGrammar
 {
+    class Calculator
+    {
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
     class Method
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-          
+            int result = Calculator.Plus(3, 4);
+            Console.WriteLine(result);
+
+            result = Calculator.Minus(5, 2);
+            Console.WriteLine(result);
         }
     }
 }
