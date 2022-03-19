a6cc2520d5b4ee41c292d16d09c4d10ec55d8755
James <woghd8503@naver.com>
2022-03-20 03:13:59 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/060_Test3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class Goto
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+
+            for (int k = 5; k > 0; k--)
+            {
+                for (int l = 0; l < k; l++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
