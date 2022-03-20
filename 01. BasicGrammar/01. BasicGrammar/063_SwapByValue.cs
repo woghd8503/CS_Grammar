66b047ed834bfc2cc69d040ae838174dabb8e628
James <woghd8503@naver.com>
2022-03-21 01:48:42 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/063_SwapByValue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _01.BasicGrammar
+{
+    class SwapByValue
+    {
+        public static void Swap(int a, int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+
+            Console.WriteLine($"x:{x}, y:{y}");
+
+            Swap(x, y);
+
+            Console.WriteLine($"x:{x}, y:{y}");
+        }
+    }
+}
