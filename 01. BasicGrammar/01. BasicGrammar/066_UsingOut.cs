202f6ebf5dc44f310d5e67ec980962c8d19469ce
James <woghd8503@naver.com>
2022-03-21 03:56:11 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/066_UsingOut.cs
+++ 01. BasicGrammar/01. BasicGrammar/066_UsingOut.cs
@@ -6,7 +6,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
-// 
+
+// ref만으로도 여러 개의 결과를 메소드에서 얻어돌 수 있지만, C#은 조금 더 안전한 방법으로 똑같은 일을 할 수
+// 있게 해줍니다. 바로 out 키워드를 이용한 "출력 전용 매개변수"가 그것입니다. out 키워드의 사용법은 간단합니다.
+// 메소드의 선언부와 호출부에 ref 키워드 대신 out 키워드를 사용하는 것이 전부입니다. 
 namespace _01.BasicGrammar
 {
     class UsingOut
