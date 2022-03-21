1c0436cc4e3979f6ee2b2d6a782f714462aa036b
James <woghd8503@naver.com>
2022-03-22 01:53:56 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/070_r .cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+// 선택적 인수
+// 
+
+namespace _01.BasicGrammar
+{
+    class NamedParameter
+    {
+        static void PrintProfile(string name, string phone)
+        {
+            Console.WriteLine($"Name:{name}, Phone:{phone}");
+        }
+
+        static void Main(string[] args)
+        {
+            PrintProfile(name: "박찬호", phone: "010-123-1234");
+            PrintProfile(phone: "010-987-9876", name: "박지성");
+            PrintProfile("박세리", "010-222-2222");
+            PrintProfile("박상현", phone: "010-567-5678");
+        }
+    }
+}
