38d932f69e2c05eca0c457d09fd3881ac8d278e3
James <woghd8503@naver.com>
2022-03-22 01:52:49 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/069_NamedParameter.cs
+++ 01. BasicGrammar/01. BasicGrammar/069_NamedParameter.cs
@@ -40,7 +40,10 @@ namespace _01.BasicGrammar
 
         static void Main(string[] args)
         {
-
+            PrintProfile(name: "박찬호", phone: "010-123-1234");
+            PrintProfile(phone: "010-987-9876", name: "박지성");
+            PrintProfile("박세리", "010-222-2222");
+            PrintProfile("박상현", phone: "010-567-5678");
         }
     }
 }
