61718c9321362ff15af45bce0e9808e1f96a1766
James <woghd8503@naver.com>
2022-03-21 23:03:10 +0900

    BasicGrammar

--- 01. BasicGrammar/01. BasicGrammar/067_Overloading.cs
+++ 01. BasicGrammar/01. BasicGrammar/067_Overloading.cs
@@ -29,24 +29,38 @@ using static System.Console;
 
 namespace _01.BasicGrammar
 {
-    class UsingOut
+    class Overloading
     {
-        static void Divide(int a, int b, out int quotient, out int remainder)
+        static int Plus(int a, int b)
         {
-            quotient = a / b;
-            remainder = a % b;
+            Console.WriteLine("Calling int Plus(int, int)...");
+            return a + b;
+        }
+
+        static int Plus(int a, int b, int c)
+        {
+            Console.WriteLine("Calling int Plus(int, int, int)...");
+            return a + b + c;
+        }
+
+        static double Plus(double a, double b)
+        {
+            Console.WriteLine("Calling double Plus(double, double)...");
+            return a + b;
+        }
+
+        static double Plus(int a, double b)
+        {
+            Console.WriteLine("Calling double Plus(int, double)...");
+            return a + b;
         }
 
         static void Main(string[] args)
         {
-            int a = 20;
-            int b = 3;
-            // int c;
-            // int d;
-
-            Divide(a, b, out int c, out int d);
-
-            Console.WriteLine($"a:{a}, b:{b}:, a/b:{c}, a%d:{d}");
+            Console.WriteLine(Plus(1, 2));
+            Console.WriteLine(Plus(1, 2, 3));
+            Console.WriteLine(Plus(1.0, 2.4));
+            Console.WriteLine(Plus(1, 2.4));
         }
     }
 }
