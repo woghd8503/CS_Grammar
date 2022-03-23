25cacdc01533a6fbf57f0b22e8d528992d82c586
James <woghd8503@naver.com>
2022-03-24 01:05:40 +0900

    BasicGrammar

---
+++ 01. BasicGrammar/01. BasicGrammar/072_Test3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace _01.BasicGrammar
+{
+    class Test3
+    {
+        static float Square(float inputNum)
+        {
+            float result = inputNum * inputNum;
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("수를 입력하세요: ");
+
+            string input = Console.ReadLine();
+            float inputNum = float.Parse(input);
+
+            Console.WriteLine("결과 : " + Square(inputNum));
+        }
+    }
+}
