79c631409f41faa784656eeff59bdefb42ab0a58
James <woghd8503@naver.com>
2022-03-25 13:46:16 +0900

    MiddleGrammar

--- 01. BasicGrammar/002.MiddleGrammar/001.BasicClass.cs
+++ 01. BasicGrammar/002.MiddleGrammar/001.BasicClass.cs
@@ -11,7 +11,7 @@ namespace _002.MiddleGrammar
         public string Name;
         public string Color;
 
-        public void Neow()
+        public void Meow()
         {
             Console.WriteLine($"{Name} : 야옹");
         }
@@ -23,8 +23,14 @@ namespace _002.MiddleGrammar
             Cat kitty = new Cat();
             kitty.Color = "하얀색";
             kitty.Name = "키티";
-            kitty.Neow();
-            Console.WriteLine($"{kitty.Name}");
+            kitty.Meow();
+            Console.WriteLine($"{kitty.Name} : {kitty.Color}");
+
+            Cat nero = new Cat();
+            nero.Color = "검은색";
+            nero.Name = "네로";
+            nero.Meow();
+
         }
     }
 }
