3f29a442ca9ca695eb14e86d8a88c4788342576f
James <woghd8503@naver.com>
2022-03-28 08:39:21 +0900

    MiddleGrammar

--- 01. BasicGrammar/002.MiddleGrammar/002.Constructor.cs
+++ 01. BasicGrammar/002.MiddleGrammar/002.Constructor.cs
@@ -8,6 +8,21 @@ namespace _002.MiddleGrammar
 {
     class Cat
     {
+        public Cat()
+        {
+            Name = "";
+            Color = "";
+        }
+
+        public Cat(string _Name, string _Color)
+        {
+            Name = _Name;
+            Color = _Color;
+        }
+
+        ~Cat()
+        {
+            Console.WriteLine($"{Name} : 잘가" };
         public string Name;
         public string Color;
 
