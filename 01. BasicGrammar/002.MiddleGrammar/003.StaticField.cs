c59171763059d5d19cd9d9be4b0e6ca3fcc86b8f
James <woghd8503@naver.com>
2022-03-28 09:05:01 +0900

    MiddleGrammar

---
+++ 01. BasicGrammar/002.MiddleGrammar/003.StaticField.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002.MiddleGrammar
+{
+    // static은 사전적으로는 정적이라는 뜻을 갖고 있습니다. 움직이지 않는다는 뜻이지요. C#에서 static은 메소드나 필드가 클래스의 인스턴스가
+    // 아닌 클래스 자체에 소속되도록 지정하는 한정자입니다.
+
+    class Global
+    {
+        public static int Count = 0;
+    }
+
+    class ClassA
+    {
+        public ClassA()
+        {
+            Global.Count++;
+        }
+    }
+
+    class ClassB
+    {
+        public ClassB()
+        {
+            //G
+        }
+    }
+}
