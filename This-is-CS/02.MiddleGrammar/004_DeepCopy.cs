0ed6a137172415b0982fe81d1f4f95ab20e57da0
James <woghd8503@naver.com>
2022-04-03 16:21:07 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/004_DeepCopy.cs
+++ This-is-CS/02.MiddleGrammar/004_DeepCopy.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _002.MiddleGrammar
 {
@@ -21,11 +17,37 @@ namespace _002.MiddleGrammar
         }
     }
 
-    claSS MainApp
+    class MainApp
     {
         static void Main(string[] args)
-    {
+        {
+            Console.WriteLine("Shallow Copy");
 
+            {
+                MyClass source = new MyClass();
+                source.MyField1 = 10;
+                source.MyField2 = 20;
 
+                MyClass target = source;
+                target.MyField2 = 30;
+
+                Console.WriteLine($"{source.MyField1} {source.MyField2}");
+                Console.WriteLine($"{target.MyField1} {source.MyField2}");
+            }
+
+            Console.WriteLine("Deep Copy");
+
+            {
+                MyClass source = new MyClass();
+                source.MyField1 = 10;
+                source.MyField2 = 20;
+
+                MyClass target = source.DeepCopy();
+                target.MyField2 = 30;
+
+                Console.WriteLine($"{source.MyField1} {source.MyField2}");
+                Console.WriteLine($"{target.MyField1} {source.MyField2}");
+            }
+        }
     }
 }
