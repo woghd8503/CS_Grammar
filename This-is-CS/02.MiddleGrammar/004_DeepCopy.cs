8fd409c7e228bb182e736ee5db81681332944dce
James <woghd8503@naver.com>
2022-04-03 23:39:01 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/004_DeepCopy.cs
+++ This-is-CS/02.MiddleGrammar/004_DeepCopy.cs
@@ -6,6 +6,12 @@
 // 얕은 복사 Shallow Copy 는 실제 객체가 아닌 스택에 있는 참조를 복사해서 받습니다.
 // 깊은 복사 Deep Copy 는 힙에 보관되어 있는 내용을 복사해서 받아 별도의 힙 공간에 객체를 보관하기를 바라는 것이죠.
 
+// ICloneable.Clone() 메소드
+// System 네임스페이스에는 ICloneable이라는 인터페이스가 있습니다. 인터페이스는 "클래스가 구현해야 하는 메소드 목록"이라고 할 수 있습니다. "깊은 복사"
+// 기능을 가질 클래스가 .NET의 다른 유틸리티 클래스나 다른 프로그래머가 작성한 코드와 호환되도록 하고 싶다면 ICloneable을 상속하는 것이 좋습니다. ICloneable
+// 인터페이스는 Clone()메소드 하나만 갖고 있습니다.
+
+
 namespace _002.MiddleGrammar
 {
     class MyClass
