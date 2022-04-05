314a8d69265d24a9e76d8524d3d49636417ffb8a
James <woghd8503@naver.com>
2022-04-06 00:43:01 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/007_.cs
+++ This-is-CS/02.MiddleGrammar/007_.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 // 객체지향 프로그래밍의 3대 특성
+// 객체지향 프로그래밍에는 여러 가지 특성이 있습니다. 그중에서도 3대 특성으로 꼽히는 것들이 있는데 이 셋중의 하나가 바로 은닉성이며
+// 나머지 2개는 상속성 inheritance과 다형성 polymorphism입니다. 
 namespace _002.MiddleGrammar
 {
     class MyClass
