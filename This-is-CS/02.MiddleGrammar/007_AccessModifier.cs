d6cf139b29f85574c64f65198b0454ff7d83f9c3
James <woghd8503@naver.com>
2022-04-06 00:54:16 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/007_AccessModifier.cs
+++ This-is-CS/02.MiddleGrammar/007_AccessModifier.cs
@@ -18,7 +18,7 @@ using System.Threading.Tasks;
 
 namespace _002.MiddleGrammar
 {
-    class WeaterHeater
+    class WaterHeater
     {
         protected int temperature;
 
@@ -36,45 +36,14 @@ namespace _002.MiddleGrammar
         {
             Console.WriteLine($"Turn on water : {temperature}");
         }
-
-        public MyClass()
-        {
-            this.a = 5425;
-            Console.WriteLine("MyClass()");
-        }
-
-        public MyClass(int b) : this()
-        {
-            this.b = b;
-            Console.WriteLine($"MyClass({b})");
-        }
-
-        public MyClass(int b, int c) : this(b)
-        {
-            this.c = c;
-            Console.WriteLine($"MyClass({b}), {c}");
-        }
-
-        public void PrintFields()
-        {
-            Console.WriteLine($"a:{a}, b:{b}, c:{c}");
-        }
     }
-
     class MainApp
     {
-        static void Main(string[] args)
+        try
         {
-            MyClass a = new MyClass();
-            a.PrintFields();
-            Console.WriteLine();
-
-            MyClass b = new MyClass(1);
-            b.PrintFields();
-            Console.WriteLine();
-
-            MyClass c = new MyClass(10, 20);
-            c.PrintFields();
+            WaterHeater heater = new WaterHeater();
         }
+
+
     }
 }
