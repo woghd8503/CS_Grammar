0a57d354ac24b0dd4287a8a88cebc5429924039e
James <woghd8503@naver.com>
2022-04-06 00:45:08 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/007_.cs
+++ This-is-CS/02.MiddleGrammar/007_.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// 객체지향 프로그래밍의 3대 특성
+// 객체지향 프로그래밍의 3대 특성 은닉성 Encapsulation
 // 객체지향 프로그래밍에는 여러 가지 특성이 있습니다. 그중에서도 3대 특성으로 꼽히는 것들이 있는데 이 셋중의 하나가 바로 은닉성이며
 // 나머지 2개는 상속성 inheritance과 다형성 polymorphism입니다. 
+
+// public            클래스의 내부/외부 모든 곳에서 접근할 수 있습니다.
+// protected         클래스의 외부에서는 접근할 수 없지만, 파생 클래스에서는 접근이 가능합니다.
+// private           클래스의 내부에서만 접근할 수 있습니다. 파생 클래
 namespace _002.MiddleGrammar
 {
     class MyClass
