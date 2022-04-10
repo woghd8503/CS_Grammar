f2030603530aff6df76be98b3dd8519b5e7f2342
James <woghd8503@naver.com>
2022-04-10 23:51:38 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/008_Inheritance.cs
+++ This-is-CS/02.MiddleGrammar/008_Inheritance.cs
@@ -7,9 +7,28 @@ using System.Threading.Tasks;
 // 
 namespace _002.MiddleGrammar
 {
-   class Base
+    class Base
     {
         protected string Name;
         public Base(string Name)
+        {
+            this.Name = Name;
+            Console.WriteLine($"{this.Name}.Base()");
+        }
+
+        ~Base()
+        {
+            Console.WriteLine($"{this.Name}.~Base()");
+        }
+
+        public void BaseMethod()
+        {
+            Console.WriteLine($"{Name}.BaseMethod()");
+        }
+    }
+
+    class Derived : Base
+    {
+        public Derived(string Name) : base
     }
 }
