a3c51de42edfc6771410c09d57fcdf2e6bed15de
James <woghd8503@naver.com>
2022-04-10 19:45:00 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/008_Inheritance.cs
+++ This-is-CS/02.MiddleGrammar/008_Inheritance.cs
@@ -7,47 +7,9 @@ using System.Threading.Tasks;
 // 
 namespace _002.MiddleGrammar
 {
-    class WaterHeater
+   class Base
     {
-        protected int temperature;
-
-        public void SetTemperature(int temperature)
-        {
-            if (temperature < -5 || temperature > 42)
-            {
-                throw new Exception("Out of temperature range");
-            }
-
-            this.temperature = temperature;
-        }
-
-        internal void TurnOnWater()
-        {
-            Console.WriteLine($"Turn on water : {temperature}");
-        }
-    }
-
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-            try
-            {
-                WaterHeater heater = new WaterHeater();
-                heater.SetTemperature(20);
-                heater.TurnOnWater();
-
-                heater.SetTemperature(-2);
-                heater.TurnOnWater();
-
-                heater.SetTemperature(50);
-                heater.TurnOnWater();
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
+        protected string Name;
+        public Base(string Name)
     }
 }
