65b388f5e5192062d0ebd188853a679c27454b06
James <woghd8503@naver.com>
2022-04-11 23:39:06 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/009_TypeCasting.cs
+++ This-is-CS/02.MiddleGrammar/009_TypeCasting.cs
@@ -20,54 +20,13 @@ using System.Threading.Tasks;
 // }
 namespace _002.MiddleGrammar
 {
-    class Base
+    class Mammal
     {
-        protected string Name;
-        public Base(string Name)
+        public void Nurse()
         {
-            this.Name = Name;
-            Console.WriteLine($"{this.Name}.Base()");
-        }
-
-        ~Base()
-        {
-            Console.WriteLine($"{this.Name}.~Base()");
-        }
-
-        public void BaseMethod()
-        {
-            Console.WriteLine($"{Name}.BaseMethod()");
+            Console.WriteLine("Nurse()");
         }
     }
 
-    class Derived : Base
-    {
-        public Derived(string Name) : base(Name)
-        {
-            Console.WriteLine($"{this.Name}.Derived()");
-        }
-
-        ~Derived()
-        {
-            Console.WriteLine($"{this.Name}.~Derived()");
-        }
-
-        public void DerivedMethod()
-        {
-            Console.WriteLine($"{Name}.DerivedMethod()");
-        }
-    }
-
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-            Base a = new Base("a");
-            a.BaseMethod();
-
-            Derived b = new Derived("b");
-            b.BaseMethod();
-            b.DerivedMethod();
-        }
-    }
+    
 }
