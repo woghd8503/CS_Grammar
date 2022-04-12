831bfcff664fa439fb8b2364a0b252097977ba64
James <woghd8503@naver.com>
2022-04-12 22:09:22 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/010_.cs
+++ This-is-CS/02.MiddleGrammar/010_.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// 객체지향 프로그래밍에서 다향성 Polymorphism은 객체가 여러 형태를 가질 수 있음을 의미합니ㅏㄷ. 객체가 범블비처럼 변신이라도 한다는 이야기는 아니지마,
+// 객체지향 프로그래밍에서 다향성 Polymorphism은 객체가 여러 형태를 가질 수 있음을 의미합니다. 객체가 범블비처럼 변신이라도 한다는 이야기는 아니지만,
 // 나름의 방법을 통해 다형성을 실현합니다. 다형성은 원래 하위 형식 다형성 subytype Polymorphism의 준말입니다. 
 namespace _002.MiddleGrammar
 {
