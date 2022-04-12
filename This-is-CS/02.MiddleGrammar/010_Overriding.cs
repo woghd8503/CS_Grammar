89508d46748eb5ba91b49f8cade071971a7c5484
James <woghd8503@naver.com>
2022-04-12 23:44:24 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/010_Overriding.cs
+++ This-is-CS/02.MiddleGrammar/010_Overriding.cs
@@ -35,54 +35,22 @@ using System.Threading.Tasks;
 
 namespace _002.MiddleGrammar
 {
-    class Mammal
+    class ArmorSuite
     {
-        public void Nurse()
+        public virtual void Initialise()
         {
-            Console.WriteLine("Nurse()");
+            Console.WriteLine("Armored");
         }
     }
 
-    class Dog : Mammal
+    class IronMan : ArmorSuite
     {
-        public void Bark()
+        public override void Initialise()
         {
-            Console.WriteLine("Bark()");
+            base.Initialise();
+            Console.WriteLine("Repulsor Rays Armed");
         }
     }
 
-    class Cat : Mammal
-    {
-        public void Meow()
-        {
-            Console.WriteLine("Meow()");
-        }
-    }
-    
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-            Mammal mammal = new Dog();
-            Dog dog;
-
-            if(mammal is Dog)
-            {
-                dog = (Dog)mammal;
-                dog.Bark();
-            }
-
-            Mammal mammal2 = new Cat();
-
-            Cat cat = mammal2 as Cat;
-            if (cat != null)
-                cat.Meow();
-
-            Cat cat2 = mammal as Cat;
-            if (cat2 != null)
-                cat2.Meow();
-            else
-                Console.WriteLine("cat2 is not a Cat");
-        }
-    }
+    class WarMachine
 }
