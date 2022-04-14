9693dabf087569ec4d5378693a57c250fc77a812
James <woghd8503@naver.com>
2022-04-14 23:58:01 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/011_MethodHiding.cs
+++ This-is-CS/02.MiddleGrammar/011_MethodHiding.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 // 토니는 장갑 수트 ArmorSuite를 개발할 때 차후에 업그레이드가 있을 것을 고려해서 Initialise() 메소드를 virtual로 선언했습니다. 
+// 오버라이딩을 통해 재정의할 수 있도록 말입니다. 어떤 메소드가 향후 오버라이딩이 될지 안 될지를 판단하는
 
 namespace _002.MiddleGrammar
 {
