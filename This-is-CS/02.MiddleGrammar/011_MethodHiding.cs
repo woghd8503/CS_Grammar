76a562ef19a381d897de0d3cf4dffad36b4a2406
James <woghd8503@naver.com>
2022-04-19 00:17:45 +0900

    MiddleGramamr

--- This-is-CS/02.MiddleGrammar/011_MethodHiding.cs
+++ This-is-CS/02.MiddleGrammar/011_MethodHiding.cs
@@ -14,5 +14,34 @@ using System.Threading.Tasks;
 
 namespace _002.MiddleGrammar
 {
-    
+    class Base
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("Base.MyMethod()");
+        }
+    }
+
+    class Derived : Base
+    {
+        public new void MyMethod()
+        {
+            Console.WriteLine("Derived.MyMethod()");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Base baseObj = new Base();
+            baseObj.MyMethod();
+
+            Derived derivedObj = new Derived();
+            derivedObj.MyMethod();
+
+            Base baseOrDerived = new Derived();
+        }
+    }
+        
 }
