a0d68f68c13008bb81a3556f24e382d95502b940
James <woghd8503@naver.com>
2022-04-20 00:03:54 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/012_SealedMethod.cs
+++ This-is-CS/02.MiddleGrammar/012_SealedMethod.cs
@@ -5,36 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 // 그동안 '상수'라는 이름을 오래도록 잊고 지냈죠? 변수는 변하는 데이터를 담지만, 상수는 변치 않는 데이터를 담습니다. 상수는 다음과 같이 const 키워드를 이용해서 선언합니다.\
-// 상수는 다음과 같이 const 키워드를 이용해서 
+// 상수는 다음과 같이 const 키워드를 이용해서 선언합니다. (기억이 돌아오지요? 자세한 내용은 3장에서 복습할 수 있습니다.)
+
+//const double pi = 3.14159265359;
+
 
 namespace _002.MiddleGrammar
 {
-    class Base
-    {
-        public virtual void SealMe()
-        {
-        }
-    }
-
-    class Derived : Base
-    {
-        public sealed override void SealMe()
-        {
-        }
-    }
-
-    class WantToOverride : Derived
-    {
-        /*public override void SealMe() // 에러 발생
-        {
-        }*/
-    }
-
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-
-        }
-    }
+   
 }
