2ec899550f6b7a96f2d4dded995ce250f60a0f51
James <woghd8503@naver.com>
2022-04-20 00:39:02 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/012_SealedMethod.cs
+++ This-is-CS/02.MiddleGrammar/012_SealedMethod.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// 그동안 '상수'라는 이름을 오래도록 잊고 지냈죠? 변수는 변하는 데이터를 담지만, 상수는 변치 않는 데이터를 담습니다. 상수는 다음과 같이 const 키워드를 이용해서 선언합니다.\
-// 상수는 다음과 같이 const 키워드를 이용해서 선언합니다. (기억이 돌아오지요? 자세한 내용은 3장에서 복습할 수 있습니다.)
+// 중첩클래스를 사용하는 이유
 
-//const double pi = 3.14159265359;
+// 1. 클래스 외부에 공개하고 싶지 않은 형식을 만들고자 할 때
+// 2. 현재클래스의 일부분처럼 표현할 수 있는 클래스를 만들고자 할 때
 
 
 namespace _002.MiddleGrammar
@@ -36,6 +36,4 @@ namespace _002.MiddleGrammar
             Configuration c = new Configuration(100, 10);
         }
     }
-
-
 }
