0940565014a955ca3a264ac4169c489e26482785
James <woghd8503@naver.com>
2022-04-20 00:00:14 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/012_SealedMethod.cs
+++ This-is-CS/02.MiddleGrammar/012_SealedMethod.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// 그동안 '상수'라는 이름을 오래도록 잊고 지냈죠? 변수는 변하는 데이터를 담지만, 상수는 변치 않는 데이터를 담습니다. 상수는 다음과 같이 const 키워드를 이용해서 선언합니다. 
+// 그동안 '상수'라는 이름을 오래도록 잊고 지냈죠? 변수는 변하는 데이터를 담지만, 상수는 변치 않는 데이터를 담습니다. 상수는 다음과 같이 const 키워드를 이용해서 선언합니다.\
+// 상수는 다음과 같이 const 키워드를 이용해서 
 
 namespace _002.MiddleGrammar
 {
