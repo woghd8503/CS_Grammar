2407ec82ddb5864843e108fb697c59568da168ec
James <woghd8503@naver.com>
2022-04-24 23:40:19 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/015_PartialClass.cs
+++ This-is-CS/02.MiddleGrammar/015_PartialClass.cs
@@ -9,73 +9,16 @@ using System.Threading.Tasks;
 
 namespace _002.MiddleGrammar
 {
-   class NestedClass
+   partial class MyClass
     {
-        List<ItemValue> listConfig = new List<ItemValue>();
-
-        public void SetConfig(string item, string value)
+        public void Method1()
         {
-            ItemValue iv = new ItemValue();
-            iv.SetValue(this, item, value);
+            Console.WriteLine("Method1");
         }
 
-        public string GetConfig(string item)
+        public void Method2()
         {
-            foreach(ItemValue iv in listConfig)
-            {
-                if (iv.GetItem() == item)
-                    return iv.GetValue();
-            }
-
-            return "";
-        }
-            
-        private class ItemValue // Configuration 클래스 안에 선언된 중첩 클래스 입니다. private으로 선언했기 때문에 Configuration 클래스 밖에서 보이지 않습니다.
-        {
-            private string item;
-            private string value;
-
-            public void SetValue(Configuration config, string item, string value)
-            {
-                this.item = item;
-                this.value = value;
-
-                bool found = false;
-                for (int i = 0; i < config.listConfig.Count; i++) // 중첩 클래스는 상위 클래스의 멤버에 자유롭게 접근할 수 있습니다.
-                {
-                    if (config.listConfigp[i].item == item)
-                    {
-                        config.listConfig[i] = this;
-                        found = true;
-                        break; 45
-                    }
-
-                }
-
-                if (found == false)
-                    config.listConfig.Add(this);
-            }
-
-            public string GetItem()
-            { return item; }
-            public string GetValue()
-            { return value;  }
-        }
-    }
-
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-            configuration config = new configuration();
-            config.SetConfig("Version", "V 5.0");
-            config.SetConfig("Size", "655.324 KB");
-
-            Console.WriteLine(config.GetConfig("Version"));
-            Console.WriteLine(config.GetConfig("Size"));
-
-            config.SetConfig("Version", "V 5.0.1");
-            Console.WriteLine(config.GetConfig("Version"));
+            Console.WriteLine();
         }
     }
 }
