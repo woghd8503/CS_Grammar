5936461115273319ae5bb8e42e855ee94447d67d
James <woghd8503@naver.com>
2022-04-25 01:38:11 +0900

    MiddleGrammar

---
+++ This-is-CS/02.MiddleGrammar/016_ExtensionMethod.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyExtension;
+
+namespace MyExtension
+{
+    public static class IntegerExtension
+    {
+        public
+    }
+}
+
+// 분할 클래스 Partial Class란, 여러 번에 나눠서 구현하는 클래스를 말합니다. 분할 클래스는 그 자체로 특별한 기능을 하는 것은 아니며 클래스의 구현이 길어질 경우
+// 여러 파일에 나눠서 구현할 수 있게 함으로써 소스 코드 관리의 편의를 제공하는 데 그 의미가 있습니다.
+
+namespace _002.MiddleGrammar
+{
+   partial class MyClass
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Method2");
+        }
+    }
+
+    partial class MyClass
+    {
+        public void Method3()
+        {
+            Console.WriteLine("Method3");
+        }
+
+        public void Method4()
+        {
+            Console.WriteLine("Method4");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            obj.Method1();
+            obj.Method2();
+            obj.Method3();
+            obj.Method4();
+        }
+    }
+}
