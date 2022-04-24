942ae9305412a74004af4c758f669ba8b2b5166e
James <woghd8503@naver.com>
2022-04-24 23:23:55 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/014_NestedClass.cs
+++ This-is-CS/02.MiddleGrammar/014_NestedClass.cs
@@ -9,21 +9,26 @@ namespace _002.MiddleGrammar
 {
     class Configuration
     {
-        private readonly int min;
+        private readonly int min; // readonly 를 이용해서 읽기 전용 필드를 선언합니다.
         private readonly int max;
 
         public Configuration(int v1, int v2)
         {
-            min = v1;
+            min = v1;            // 읽기 전용 필드는 생성자 안에서만 초기화 가능합니다.
             min = v2;
         }
 
         public void ChangeMin(int newMax)
         {
-            max = newMax;      // 
+            max = newMax;      // 생성자가 아닌 다른 곳에서 값을 수정하려 하면 컴파일 에러가 발생합니다.
         }
     }
    
-
-
+    class MainApp
+    {
+        static void Main(string[] srgs)
+        {
+            Configuration c = new Configuration(100, 10);
+        }
+    }
 }
