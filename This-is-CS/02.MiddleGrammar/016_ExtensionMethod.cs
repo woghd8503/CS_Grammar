42540f4dd6d9a135e9df4caf8c70383a47935441
James <woghd8503@naver.com>
2022-04-25 01:39:22 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/016_ExtensionMethod.cs
+++ This-is-CS/02.MiddleGrammar/016_ExtensionMethod.cs
@@ -9,8 +9,12 @@ namespace MyExtension
 {
     public static class IntegerExtension
     {
-        public
+        public static int Square(this int myInt)
+        {
+            return myInt * myInt;
+        }
     }
+
 }
 
 // 분할 클래스 Partial Class란, 여러 번에 나눠서 구현하는 클래스를 말합니다. 분할 클래스는 그 자체로 특별한 기능을 하는 것은 아니며 클래스의 구현이 길어질 경우
