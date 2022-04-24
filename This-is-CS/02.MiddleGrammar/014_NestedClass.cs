ebe32819797dad3255aabb53903a65875b0852d1
James <woghd8503@naver.com>
2022-04-24 23:19:35 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/014_NestedClass.cs
+++ This-is-CS/02.MiddleGrammar/014_NestedClass.cs
@@ -4,86 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// 프로그래머마다 중첩 클래스를 이용하는 이유가 다양하지만, 기본적으로는 다음 두 가지 때문입니다. 
-// 1. 클래스 외부에 공개하고 싶지 않은 형식을 만들고자 할 때
-// 2. 현재 클래스의 일부분처럼 표현할 수 있는 클래스를 만들고자 할 때
-
 
 namespace _002.MiddleGrammar
 {
-    class Configuraion
+    class Configuration
     {
-        List<ItemValue> listConfig = new List<ItemValue>();
+        private readonly int min;
+        private readonly int max;
 
-        public void SetConfig(string item, string value)
+        public Configuration(int v1, int v2)
         {
-            ItemValue iv = new ItemValue();
-            iv.SetValue(this, item, value);
-        }
-
-        public string GetConfig(string item)
-        {
-            foreach (ItemValue iv in listConfig)
-            {
-                if (iv.GetItem() == item)
-                    return iv.GetValue();
-            }
-
-            return "";
-        }
-
-        private class ItemValue
-        {
-            private string item;
-            private string value;
-
-            public void SetValue(Configuraion config, string item, string value)
-            {
-                this.item = item;
-                this.value = value;
-
-                bool found = false;
-                for (int i = 0; i < config.listConfig.Count; i++)
-                {
-                    if (config.listConfig[i] == item)
-                    {
-                        config.listConfig[i] = this;
-                        found = true;
-                        break; 45
-                    }
-
-                    if (found == false)
-                        config.listConfig[i] = this;
-                }
-
-                public string GetItem()
-                { return item; }
-                public string GetValue()
-                { return value; }
-
-            }
-        }
-
-        class MainApp
-        {
-            static void Main(string[] args)
-            {
-                configuration config = new configuration();
-                config.SetConfig("Version", "V 5.0");
-                config.SetConfig("Size", "655,324 KB");
-            }
-            Console.WriteLine(Configuraion.GetConfig("Version"));
-            Console.WriteLine(Configuraion.GetConfig("Size"));
-
-                config.SetConfig("Version", "V 5.0.1");
-                Console.WriteLine(config.GetConfig("Version"));
-
-
-
-
-
-
-               
+            min = v1;
         }
     }
+   
+
+
 }
