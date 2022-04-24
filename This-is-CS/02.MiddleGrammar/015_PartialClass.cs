b37fbd37500241cb8399be608cd8fb2a2ee4c56b
James <woghd8503@naver.com>
2022-04-24 23:39:03 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/015_PartialClass.cs
+++ This-is-CS/02.MiddleGrammar/015_PartialClass.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// 분할 클래스 Partial Class란, 여러 번에 나눠서 구현하는 클래스를 말합니다. 분할 클래스는 그 자체로 특별한 기능을 하는 것은 아니며
+// 분할 클래스 Partial Class란, 여러 번에 나눠서 구현하는 클래스를 말합니다. 분할 클래스는 그 자체로 특별한 기능을 하는 것은 아니며 클래스의 구현이 길어질 경우
+// 여러 파일에 나눠서 구현할 수 있게 함으로써 소스 코드 관리의 편의를 제공하는 데 그 의미가 있습니다.
 
 namespace _002.MiddleGrammar
 {
