a9a16342bd314d97fcd33e46e1dae064c556557a
James <woghd8503@naver.com>
2022-04-26 01:57:10 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/017_Structure.cs
+++ This-is-CS/02.MiddleGrammar/017_Structure.cs
@@ -34,35 +34,30 @@ namespace Struct
             this.Z = Z;
         }
 
-        public 
-    }
-    public static class IntegerExtension
-    { 
-        public static int Square(this int myInt)
+        public override string ToString()
         {
-            return myInt * myInt;
-        }
+            return string.Format($"{X}, {Y}, {Z}");
 
-        public static int Power(this int myInt, int exponent)
-        {
-            int result = myInt;
-            for (int i = 1; i < exponent; i++)
-                result = result * myInt;
-
-            return result;
         }
     }
-}
 
-namespace ExtensionMethod
-{
     class MainApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"3^2 : {3.Square()}");
-            Console.WriteLine($"3^4 : {3.Power(4)}");
-            Console.WriteLine($"2^10 : {2.Power(10)}");
+            Point3D p3d1;
+            p3d1.X = 10;
+            p3d1.Y = 20;
+            p3d1.Z = 40;
+
+            Console.WriteLine(p3d1.ToString());
+
+            Point3D p3d2 = new Point3D(100, 200, 300);
+            Point3D p3d3 = p3d2;
+            p3d3.Z = 400;
+
+            Console.WriteLine(p3d2.ToString());
+            Console.WriteLine(p3d3.ToString());
         }
     }
 }
