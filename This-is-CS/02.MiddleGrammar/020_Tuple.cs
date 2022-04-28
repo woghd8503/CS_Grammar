1304cb616d0dc766d03d928b5c7a5f3a69fc1a01
James <woghd8503@naver.com>
2022-04-29 01:09:18 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/020_Tuple.cs
+++ This-is-CS/02.MiddleGrammar/020_Tuple.cs
@@ -4,10 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// 튜플 Tuple도 여러 필드를 담을 수 있는 구조체입니다. 하지만 앞서 살펴봤던 구조체와는 달리 튜플은
+// 형식 이름이 없습니다. 그래서 튜플은 응용 프로그램 전체에서 사용할 형식을 선언할 대가 아닌, 즉
+// 석에서 사용할 복합 데이터 형식을 선언할 때 적합합니다. 튜플은 구조체이므로 값 형식입니다. 값
+// 형식은 생성된 지역을 벗어나면 스택에서 소멸되기 때문에 프로그램에 장기적인 부담을 주지 않는 
+// 다는 장점이 있죠.
 namespace Tuple
 {
     class MainApp
     {
-        static
+        static void Main(string[] args)
+        {
+            // 명명되지 않은 튜플
+            var a = { "슈펴맨", 9999 };
+        }
     }
 }
