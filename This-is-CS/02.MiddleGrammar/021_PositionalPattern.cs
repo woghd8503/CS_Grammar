26615497c30075ac03c62a6c67373c2f6d765763
James <woghd8503@naver.com>
2022-05-01 01:47:04 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/021_PositionalPattern.cs
+++ This-is-CS/02.MiddleGrammar/021_PositionalPattern.cs
@@ -5,7 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 // 튜플이 분해가 가능한 이유는 분해자 Deconstructor를 구현하고 있기 때문인데요. 분해자를 구현하고 있는 객체를 분해한 결과를
-// switch문이나
+// switch문이나 switch 식의 분기 조건에 활용할 수 있습니다. 이것을 어려운 말로 위치 패턴 매칭 Positional Pattern Matching이라고 합니다.
+// 식별자나 데이터 형식이 아닌 분해된 요소의 위치에 따라 값이 일치하는지를 판단하는 것이죠. 
 
 namespace Tuple
 {
