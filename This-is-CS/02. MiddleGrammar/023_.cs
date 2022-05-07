8771b167db0dfa6847fa66b779ca7f7870147081
James <woghd8503@naver.com>
2022-05-07 23:54:56 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/023_.cs
+++ This-is-CS/02. MiddleGrammar/023_.cs
@@ -4,22 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// 인터페이스 interface가 무엇인지 설명하기 전에, 인터페이스가 어떻게 생겼는지 알아보겠습니다. 인터페이스는 다음과 
+// 같이 interface 키워드를 이용해서 선언합니다.
 
 namespace Test1
 {
     class MainApp
     {
-        // Q1. 클래스와 객체, 인스턴스는 서로 어떤 점이 다른가요?
-
-        // Q2. 다음 코드에서 오류를 찾고, 오류의 원인을 설명하세요.
-
-        // Q3. this키워드와 base 키워드에 대해 설명하세요.
-
-        // Q4. 구조체에 대한 다음 설명 중 틀린 것을 모두 찾으세요.
-        //    1. struct 키워드를 이용하여 선언한다.
-        //    2. 복사할 때 얕은 목사가 이루어진다.
-        //    3. 참조 형식이다.
-        //    4. 메소드를 가질 수 있다.
 
 
     }
