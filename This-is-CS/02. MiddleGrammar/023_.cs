b66db386d1c94d94306cd63f268db47ba124d90c
James <woghd8503@naver.com>
2022-05-07 23:52:42 +0900

    MiddleGrammar

---
+++ This-is-CS/02. MiddleGrammar/023_.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Test1
+{
+    class MainApp
+    {
+        // Q1. 클래스와 객체, 인스턴스는 서로 어떤 점이 다른가요?
+
+        // Q2. 다음 코드에서 오류를 찾고, 오류의 원인을 설명하세요.
+
+        // Q3. this키워드와 base 키워드에 대해 설명하세요.
+
+        // Q4. 구조체에 대한 다음 설명 중 틀린 것을 모두 찾으세요.
+        //    1. struct 키워드를 이용하여 선언한다.
+        //    2. 복사할 때 얕은 목사가 이루어진다.
+        //    3. 참조 형식이다.
+        //    4. 메소드를 가질 수 있다.
+
+
+    }
+
+    // Q5. 다음 코드를 컴파일 및 실행이 가능하도록 수정하세요.
+    struct ACSetting
+    {
+        public double currentInCelsius; // 현재 온도 ( 도시)
+        public double target;           // 희망 온도
+
+        public readonly double GetFahrenheit()
+        {
+            target = currentInCelsius * 1.8 + 32; // 화씨 ( F) 계산 결과를 target에 저장
+            return target; // target 반환
+        }
+    }
+
+}
+
