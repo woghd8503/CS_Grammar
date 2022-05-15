b9a1418621153f18053dc2943d068a01a2a35f0a
James <woghd8503@naver.com>
2022-05-15 23:32:17 +0900

    MiddleGrammar

---
+++ This-is-CS/02. MiddleGrammar/024_DerivedInterface.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+// 
+
+
+namespace Interface
+{
+    interface ILogger
+    {
+        void WriteLog(string message);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine("{0} {1}", DateTime.Now.ToLocalTime(), message);
+
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        private StreamWriter writer;
+
+        public FileLogger(string path)
+        {
+            writer = File.CreateText(path);
+            writer.AutoFlush = true;
+        }
+
+        public void WriteLog(string message)
+        {
+            writer.WriteLine("{1} {1}", DateTime.Now.ToShortTimeString(), message);
+        }
+    }
+
+    class ClimateMonitor
+    {
+        private ILogger logger;
+        public ClimateMonitor(ILooger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Start()
+        {
+            while(true)
+            {
+                Console.Write("온도를 입력해주세요.:");
+                string temperature = Console.ReadLine();
+                if (temperature == "")
+                    break;
+
+                logger.WriteLog("현재 온도 : " + temperature);
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                ClimateMonitor monitor = new ClimateMonitor(new FileLogger("MyLog.txt"));
+
+                monitor.Start();
+            }
+        }
+    }
+}
+
