72b687fea3e1dc6fe356a0ddc07b72e2e9fc8b21
James <woghd8503@naver.com>
2022-05-19 03:03:47 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/025_MultiInterfaceInheritance .cs
+++ This-is-CS/02. MiddleGrammar/025_MultiInterfaceInheritance .cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-// 클래스는 여러 클래스를 한꺼번에 상속할 수 없습니다. 이른바 "죽음의 다이아몬드"라는 문제 때문입니다. 죽음의 다이아몬드란, 최초의 클래스 (Ridable)
+// 클래스는 여러 클래스를 한꺼번에 상속할 수 없습니다. 이른바 "죽음의 다이아몬드"라는 문제 때문입니다. 죽음의 다이아몬드란, 최초의 클래스 (Ridable)가 두 개의 파생 클래스
 
 
 namespace DerivedInterface
