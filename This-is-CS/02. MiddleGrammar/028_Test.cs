36c55901cb42e7e40d7542e4afcbe8be6ca88080
James <woghd8503@naver.com>
2022-05-31 08:39:01 +0900

    MiddleGrammar

---
+++ This-is-CS/02. MiddleGrammar/028_Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Test
+{
+    // 인터페이스와
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
+
+
