4a62511c0d42872e4a3e6cc5c729892cdb3219b9
James <woghd8503@naver.com>
2022-06-01 22:18:52 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/029_Property.cs
+++ This-is-CS/02. MiddleGrammar/029_Property.cs
@@ -50,6 +50,16 @@ namespace Property
 
     }
 
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo();
+            birth.Name = "서현";
+            birth.Birthday = new DateTime(1991, 6, 28);
+            Console.WriteLine($"Name :  {birth.Name}");
+        }
+    }
 
 }
 
