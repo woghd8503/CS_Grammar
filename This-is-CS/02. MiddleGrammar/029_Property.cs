cd96e0d111440c6d88ad006fbe049326327ba05a
James <woghd8503@naver.com>
2022-06-01 21:51:03 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/029_Property.cs
+++ This-is-CS/02. MiddleGrammar/029_Property.cs
@@ -5,7 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-// 프로퍼티 선언 문법에서 get {...}과 set {...}
+// 프로퍼티 선언 문법에서 get {...}과 set {...}을 일컬어 접근자 accessor라고 합니다. get 접근자는 필드로부터 값을 읽어오고 set 접근자는 필드에 값을 할당합니다.
+
 
 namespace Property
 {
