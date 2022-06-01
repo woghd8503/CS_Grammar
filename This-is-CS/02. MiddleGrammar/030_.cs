2cef1c620f872e33ad183b8c05e3060efe853026
James <woghd8503@naver.com>
2022-06-01 22:27:47 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/030_.cs
+++ This-is-CS/02. MiddleGrammar/030_.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-// 프로퍼티는 데이터의 오염에 대해선 메소드처럼 안전하고,
+// 프로퍼티는 데이터의 오염에 대해선 메소드처럼 안전하고, 데이터를 다룰 때는 필드처럼 간결합니다. 하지만, 많은 경우에 중복된 코드를 작성하고 있다는 기분이 들게 합니다.
 
 namespace Property
 {
