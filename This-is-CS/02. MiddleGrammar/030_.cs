22431e30460079000f1b45488233e0db661251b7
James <woghd8503@naver.com>
2022-06-01 22:39:36 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/030_.cs
+++ This-is-CS/02. MiddleGrammar/030_.cs
@@ -22,22 +22,16 @@ namespace AutoImplementedProperty
         {
             get
             {
-                return new DateTime(DateTime.Now.Subtract())
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
             }
         }
-
     }
 
     class MainApp
     {
         static void Main(string[] args)
         {
-            BirthdayInfo birth = new BirthdayInfo();
-            birth.Name = "서현";
-            birth.Birthday = new DateTime(1991, 6, 28);
-            Console.WriteLine($"Name : {birth.Name}");
-            Console.WriteLine($"Birthday : {birth.Birthday.ToShortDateString()}");
-            Console.WriteLine($"Age : {birth.Age}");
+        
         }
     }
 
