3f8c7828e55db3c0334315a2a1f2d7526b53f552
James <woghd8503@naver.com>
2022-06-01 21:50:18 +0900

    MiddleGrammar

---
+++ This-is-CS/02. MiddleGrammar/029_Property.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+// 프로퍼티 선언 문법에서 get {...}과 set {...}
+
+namespace Property
+{
+
+
+}
+
+
