c03600197fbcd4418b6b5c9d0b879448cbec810d
James <woghd8503@naver.com>
2022-06-06 12:00:48 +0900

    MiddleGrammar

---
+++ This-is-CS/02. MiddleGrammar/031_ConstructorWithProperty.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ConstructorWithProperty
+{
+    interface IRunnable
+    {
+        void Run();
+    }
+
+    interface IFlyable
+    {
+        void Fly();
+    }
+
+    class FlyingCar : IRunnable, IFlyable
+    {
+        public void Run()
+        {
+            Console.WriteLine("Run! Run!");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("Fly! Fly!");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            FlyingCar Car = new FlyingCar();
+            Car.Run();
+            Car.Fly();
+
+            IRunnable runnable = Car as IRunnable;
+            runnable.Run();
+
+            IFlyable flyable = Car as IFlyable;
+            flyable.Fly();
+        }
+    }
+
+}
+
