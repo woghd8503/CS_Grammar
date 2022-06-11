ee7b0e1225eadf16c9cd2ec8e1abb884e486f803
James park <woghd8503@naver.com>
2022-06-11 20:22:54 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/032_.cs
+++ This-is-CS/02. MiddleGrammar/032_.cs
@@ -5,7 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-// 의도치 않게 데이터가 오염되는 
+// 의도치 않게 데이터가 오염되는 일이 종종 있는데 C#에는 데이터 오염을 방지할 수 있는 장치가 여럿 있습니다. 접근 한정자, readonly 필드, readonly 구조체, 튜플 등이
+// 그 예죠.
 
 namespace ConstructorWithProperty
 {
