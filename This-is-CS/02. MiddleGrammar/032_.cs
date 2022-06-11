fe078aa9e30ec58d22f4d561be880d3a313e5388
James park <woghd8503@naver.com>
2022-06-11 20:21:54 +0900

    MiddleGrammar

---
+++ This-is-CS/02. MiddleGrammar/032_.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+// 의도치 않게 데이터가 오염되는 
+
+namespace ConstructorWithProperty
+{
+    class BirthdayInfo
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public DateTime Birthday
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo()
+            { Name = "서현",
+              Birthday = new DateTime(1991, 6, 28)
+            };
+
+            Console.WriteLine($"Name : {birth.Name}");
+            Console.WriteLine($"BirthDay : {birth.Name}");
+            Console.WriteLine($"Age : {birth.Age}");
+        }
+    }
+
+}
+
