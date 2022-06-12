92638e89cc7846aa12f4b4da852d9248c78492ad
James <woghd8503@naver.com>
2022-06-13 01:05:13 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/033_.cs
+++ This-is-CS/02. MiddleGrammar/033_.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 
 
-// 불변 immutable 객체는 내부 상태 데이터를 변경할 수 없는 객체를 말하는데요.
+// 불변 immutable 객체는 내부 상태 데이터를 변경할 수 없는 객체를 말하는데요. 상태를 변경할 수 없다는 특성 때문에 불변 객체에서는 데이터 복사와 비교가
+// 많이 이뤄집니다. 새로운 
 namespace InitOnly
 {
     class Transaction
