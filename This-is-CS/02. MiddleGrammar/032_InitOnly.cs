bb329c84a1a8fa28a98920532e782abdeb7622a9
James <woghd8503@naver.com>
2022-06-13 00:07:31 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/032_InitOnly.cs
+++ This-is-CS/02. MiddleGrammar/032_InitOnly.cs
@@ -28,8 +28,9 @@ namespace InitOnly
     {
         static void Main(string[] args)
         {
-            Transaction tr1 = new Transaction {From="Alice", To="Bob", Amount=100};
-            Transaction tr2 = new 
+            Transaction tr1 = new Transaction { From = "Alice", To = "Bob", Amount = 100 };
+            Transaction tr2 = new Transaction { From = "Bob", To = "Charlie", Amount = 50 };
+            Transaction tr3 = new Transaction { From = "Charlie", To = "Alice", Amount = 50 };
 
             Console.WriteLine($"Name : {birth.Name}");
             Console.WriteLine($"BirthDay : {birth.Name}");
