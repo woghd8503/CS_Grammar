e43273e09e509b7f5e17e939269992b7fb6371e9
James <woghd8503@naver.com>
2022-06-13 01:08:10 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/033_.cs
+++ This-is-CS/02. MiddleGrammar/033_.cs
@@ -6,7 +6,7 @@
 
 namespace Record
 {
-    class Transaction
+    record RTransaction
     {
         public string From { get; init; }
         public string To { get; init; }
@@ -22,9 +22,9 @@ namespace Record
     {
         static void Main(string[] args)
         {
-            Transaction tr1 = new Transaction { From = "Alice", To = "Bob", Amount = 100 };
-            Transaction tr2 = new Transaction { From = "Bob", To = "Charlie", Amount = 50 };
-            Transaction tr3 = new Transaction { From = "Charlie", To = "Alice", Amount = 50 };
+            RTransaction tr1 = new RTransaction { From = "Alice", To = "Bob", Amount = 100 };
+            RTransaction tr2 = new RTransaction { From = "Bob", To = "Charlie", Amount = 50 };
+            RTransaction tr3 = new RTransaction { From = "Charlie", To = "Alice", Amount = 50 };
 
             Console.WriteLine(tr1);
             Console.WriteLine(tr2);
