02536f447f08ae31aaf5545f97fccaf5040ad98d
James <woghd8503@naver.com>
2022-06-19 11:16:33 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/034_WithExp.cs
+++ This-is-CS/02. MiddleGrammar/034_WithExp.cs
@@ -26,7 +26,7 @@ namespace WithExp
         {
             RTransaction tr1 = new RTransaction{From = "Alice", To = "Bob", Amount = 100};
             RTransaction tr2 = tr1 with { To = "Charlie" };
-            RTransaction tr3 = tr2 with { From = "Charlie" };
+            RTransaction tr3 = tr2 with { From = "Dabe", Amount=30 };
 
             Console.WriteLine(tr1);
             Console.WriteLine(tr2);
