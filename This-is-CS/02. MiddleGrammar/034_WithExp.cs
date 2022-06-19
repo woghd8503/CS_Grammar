5c7ab9a29f2ef5fe57e2379ad744810b1f26c8ee
James <woghd8503@naver.com>
2022-06-20 01:01:00 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/034_WithExp.cs
+++ This-is-CS/02. MiddleGrammar/034_WithExp.cs
@@ -4,7 +4,6 @@ using System.Runtime.CompilerServices;
 // 불변 immutable 객체는 내부 상태 데이터를 변경할 수 없는 객체를 말하는데요. 상태를 변경할 수 없다는 특성 때문에 불변 객체에서는 데이터 복사와 비교가
 // 많이 이뤄집니다. 새로운 상태를 표현하려고 기존 상태를 복사한 뒤 이 중 일부를 수정해서 새로운 객체를 만들고, 상태를 확인하기 위해 객체내용을 작주 비교합니다.
 
-
 namespace WithExp
 {
     record RTransaction
