2fefc7c1811c56c68147c9846a8b72721b121c4b
James <woghd8503@naver.com>
2022-06-27 23:48:20 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/035_RecordComp.cs
+++ This-is-CS/02. MiddleGrammar/035_RecordComp.cs
@@ -19,6 +19,14 @@ namespace RecordComp
     {
         public string From { get; init; }
         public string To { get; init; }
+        public int Amount { get; init; }
+
+        public override string ToString()
+        {
+            return $"{From,-10} -> {To,-10} : ${Amount}";
+        }
+    }
+
         static void Main(string[] args)
         {
             RTransaction tr1 = new RTransaction{From = "Alice", To = "Bob", Amount = 100};
