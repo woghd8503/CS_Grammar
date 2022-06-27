e5f9b1108243773f521a7a2f53a9903aa2f8de7b
James <woghd8503@naver.com>
2022-06-27 23:54:17 +0900

    MiddleGramamr

--- This-is-CS/02. MiddleGrammar/035_RecordComp.cs
+++ This-is-CS/02. MiddleGrammar/035_RecordComp.cs
@@ -32,6 +32,20 @@ namespace RecordComp
         static void Main(string[] args)
         {
             CTransaction trA = new CTransaction { From = "Alice", To = "Bob", Amount = 100 };
+
+            CTransaction trB = new CTransaction { From = "Alice", To = "Bob", Amount = 100 };
+
+            Console.WriteLine(trA);
+            Console.WriteLine(trB);
+            Console.WriteLine($"trA equals to trB : {trA.Equals(trB)}");
+
+            RTransaction tr1 = new RTransaction { From = "Alice", To = "Bob", Amount = 100 };
+
+            RTransaction tr2 = new RTransaction { From = "Alice", To = "Bob", Amount = 100 };
+
+            Console.WriteLine(tr1);
+            Console.WriteLine(tr2);
+            Console.WriteLine($"tr1 equals to tr2 : {tr1.Equals(tr2)}");
         }
     }
 }
