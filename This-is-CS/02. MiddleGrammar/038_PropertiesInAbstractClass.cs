0d5d8d48fa6c6719a50523892ba981ca3f6b1377
James <woghd8503@naver.com>
2022-07-10 23:28:05 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/038_PropertiesInAbstractClass.cs
+++ This-is-CS/02. MiddleGrammar/038_PropertiesInAbstractClass.cs
@@ -34,6 +34,11 @@ namespace PropertiesInAbstractClass
             { ProductDate = new DateTime(2018, 1, 10) };
 
             Console.WriteLine("Product:{0}, Product Date :{1}", product_1.SerialID, product_1.ProductDate);
+
+            Product product_2 = new MyProduct()
+            { ProductDate = new DateTime(2018, 2, 3) };
+
+            Console.WriteLine("Product:{0}, Product Date :{1}", product_2.SerialID, product_2.ProductDate);
         }
     }
 
