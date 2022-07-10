5cb65de918b8d57454d337a4cbd7974f9782e12d
James <woghd8503@naver.com>
2022-07-10 23:19:13 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/038_PropertiesInAbstractClass.cs
+++ This-is-CS/02. MiddleGrammar/038_PropertiesInAbstractClass.cs
@@ -5,6 +5,13 @@ namespace PropertiesInAbstractClass
     abstract class Product
     {
         private class Product
+        {
+            private static int serial = 0;
+            public string SerialID
+            {
+                get { return String.Format("{}")}
+            }
+        }
     }
 
 }
