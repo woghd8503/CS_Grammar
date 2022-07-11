a9abe9d061684e826fae4e190c22305935ac3975
James <woghd8503@naver.com>
2022-07-12 02:20:06 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/039_Test.cs
+++ This-is-CS/02. MiddleGrammar/039_Test.cs
@@ -2,7 +2,7 @@
 
 namespace Test
 {
-    // 다음 코드에서 NameCard 클래스의 GetAge(), SetAge(), 
+    // 다음 코드에서 NameCard 클래스의 GetAge(), SetAge(), GetName(), SetName() 메소드들을 프로퍼티로 변경해 작성하세요.
     class NameCard
     {
         private int age;
