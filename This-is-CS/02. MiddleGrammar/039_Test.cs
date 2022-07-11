868c75a2da7a4563ebfab6c0cd89c889b6f014f6
James <woghd8503@naver.com>
2022-07-12 02:18:54 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/039_Test.cs
+++ This-is-CS/02. MiddleGrammar/039_Test.cs
@@ -10,7 +10,28 @@ namespace Test
         public int GetAge()
         { return age; }
 
+        public void SetAge(int value)
+        { age = value; }
 
+        public string GetName()
+        { return name; }
+
+        public void SetName(string value)
+        { name = value; }
+    }
+
+    class MainApp
+    {
+        public static void Main()
+        {
+            NameCard MyCard = new NameCard();
+
+            MyCard.SetAge(24);
+            MyCard.SetName("상현");
+
+            Console.WriteLine("나이 : {0}", MyCard.GetAge());
+            Console.WriteLine("이름 : {0}", MyCard.GetName());
+        }
     }
 
 }
