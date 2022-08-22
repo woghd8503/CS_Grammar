7bae99c53ede4ccd629ee53442f64d99e7ea9c9b
James <woghd8503@naver.com>
2022-08-22 23:56:07 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/ArraySample.cs
+++ This-is-CS/02. MiddleGrammar/ArraySample.cs
@@ -8,6 +8,17 @@ namespace _002.MiddleGrammar
 {
     class ArraySample
     {
-
+        static void Main(string[] args)
+        {
+            int[] scores = new int[5];
+            scores[0] = 80;
+            scores[1] = 74;
+            scores[2] = 81;
+            scores[3] = 90;
+            scores[4] = 34;
+            
+            foreach(int score in scores)
+                
+        }
     }
 }
