417a6e45a885f7a779d798ee1c4a8ee813002640
James <woghd8503@naver.com>
2022-08-22 23:57:48 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/ArraySample.cs
+++ This-is-CS/02. MiddleGrammar/ArraySample.cs
@@ -18,7 +18,16 @@ namespace _002.MiddleGrammar
             scores[4] = 34;
             
             foreach(int score in scores)
-                
+                Console.WriteLine(score);
+
+            int sum = 0;
+            foreach (int score in scores)
+                sum += score;
+
+            int average = sum / scores.Length;
+
+            Console.WriteLine($"Average Score : {average}");
         }
     }
+
 }
