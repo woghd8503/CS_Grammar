7c9d3d5f4ba6929e6d91a8327d22213042fb113d
James <woghd8503@naver.com>
2022-09-04 21:50:38 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/043_DerivedFromArray.cs
+++ This-is-CS/02.MiddleGrammar/043_DerivedFromArray.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// 분류                /            이름              /
+// 정적메소드                       Sort()                         배열을 정렬합니다.
+//                            BinarySearch<T>()         이진 탐색을 수행합니다. 꺽쇠 과효<, >와 그 사이에 있는 T에 대해선 잠시 후에 설명하겠습니다.          
 namespace _0002.MiddleGrammar
 {
     class _043_DerivedFromArray
