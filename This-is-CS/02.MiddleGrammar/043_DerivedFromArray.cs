bb822a64a2e06a7d8c0a4fbd82478eeba9c75a3a
James <woghd8503@naver.com>
2022-09-04 21:47:37 +0900

    MiddleGrammar

---
+++ This-is-CS/02.MiddleGrammar/043_DerivedFromArray.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002.MiddleGrammar
+{
+    class _043_DerivedFromArray
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 10, 30, 20, 7, 1 };
+            Console.WriteLine($"Type Of array : {array.GetType()}");
+            Console.WriteLine($"Base type Of array : {array.GetType().BaseType}");
+        }
+    }
+}
