186f27b26d9cb4634f746ff10cb10a967fe48e1f
James <woghd8503@naver.com>
2022-09-04 22:04:11 +0900

    MiddleGrammar

---
+++ This-is-CS/02.MiddleGrammar/MoreOnArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002.MiddleGrammar
+{
+    class MoreOnArray
+    {
+        private static bool CheckPassed(int score)
+        {
+            return score >= 60;
+        }
+
+        private static void Print(int value)
+        {
+            Console.Write($"{value}");
+        }
+
+        static void Main(string[] args)
+        {
+            int[] scores = new int[] { 80, 74, 81, 90, 34 };
+
+            foreach(int score in scores)
+                Console.Write($"{score} ");
+            Console.WriteLine();
+
+            Array.Sort(scores);
+            Array.ForEach<int>(scores, new Action<int>(Print));
+            Console.WriteLine();
+
+            Console.WriteLine($"Number of dimensions : {scores.Rank}");
+
+            Console.WriteLine($"Binary Search : 81 is at " +
+                $"{Array.BinarySearch<int>(scores, 81)}");
+
+            Console.WriteLine($"Linear Search : 90 is at " +
+                $"{Array}");
+
+        }
+    }
+}
