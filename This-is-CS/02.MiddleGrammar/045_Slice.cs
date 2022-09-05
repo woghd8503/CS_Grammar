bfb34d3d9ce737ae6a0862d3554ccf23c41288f3
James <woghd8503@naver.com>
2022-09-05 23:18:45 +0900

    MiddleGrammar

---
+++ This-is-CS/02.MiddleGrammar/045_Slice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002.MiddleGrammar
+{
+    class _045_Slice
+    {
+        static void PrintArray(System.Array)
+    }
+}
