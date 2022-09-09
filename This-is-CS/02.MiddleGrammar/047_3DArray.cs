f244ddccc7d699000cae83ae0a2d8f86566aee3d
James <woghd8503@naver.com>
2022-09-09 23:58:36 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/047_3DArray.cs
+++ This-is-CS/02.MiddleGrammar/047_3DArray.cs
@@ -22,7 +22,7 @@ namespace _0002.MiddleGrammar
             {
                 for(int j = 0; j < array.GetLength(1); j++)
                 {
-                    Console.WriteLine();
+                    Console.Write("{ ");
                 }
             }
         }
