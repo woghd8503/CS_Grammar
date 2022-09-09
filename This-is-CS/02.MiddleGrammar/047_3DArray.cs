13c400e6774fcdae77383a0e75d0e62a02fecf3e
James <woghd8503@naver.com>
2022-09-09 23:59:37 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/047_3DArray.cs
+++ This-is-CS/02.MiddleGrammar/047_3DArray.cs
@@ -23,7 +23,7 @@ namespace _0002.MiddleGrammar
                 for(int j = 0; j < array.GetLength(1); j++)
                 {
                     Console.Write("{ ");
-                    for(int j = 0; j < array.GetLength(1); j++)
+                    for(int k = 0; j < array.GetLength(1); j++)
                     {
 
                     }
