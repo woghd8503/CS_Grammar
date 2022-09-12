f69138b14576c01a30897789727202b6e1a0e399
James <woghd8503@naver.com>
2022-09-12 23:45:03 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/048_JaggedArray.cs
+++ This-is-CS/02.MiddleGrammar/048_JaggedArray.cs
@@ -8,5 +8,21 @@ namespace _0002.MiddleGrammar
 {
     class _048_JaggedArray
     {
+        static void Main(string[] args)
+        {
+            int[][] jagged = new int[3][];
+            jagged[0] = new int[5] { 1, 2, 3, 4, 5 };
+            jagged[1] = new int[] { 10, 20, 30 };
+            jagged[2] = new int[] { 100, 200 };
+
+            foreach (int[] arr in jagged)
+            {
+                Console.Write($"Length : {arr.Length}, ");
+                foreach(int e in arr)
+                {
+                    Console.Write($"{e} ");
+                }
+            }
+        }
     }
 }
