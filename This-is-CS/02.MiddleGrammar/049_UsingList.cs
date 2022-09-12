9a65dd1bb4faafb98944489130062148820aaaf9
James <woghd8503@naver.com>
2022-09-12 23:52:26 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/049_UsingList.cs
+++ This-is-CS/02.MiddleGrammar/049_UsingList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,18 @@ namespace _0002.MiddleGrammar
 {
     internal class _049_UsingList
     {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            for (int i = 0; i < 5; i++)
+                list.Add(i);
+
+            foreach (object obj in list)
+                Console.Write($"{obj} ");
+            Console.WriteLine();
+
+            list.RemoveAt(2);
+
+        }
     }
 }
