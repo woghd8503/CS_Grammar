9a70bd70d6aa02f5c9cc843d623bd001057d3bb7
James <woghd8503@naver.com>
2022-09-12 23:39:40 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/045_Slice.cs
+++ This-is-CS/02.MiddleGrammar/045_Slice.cs
@@ -28,8 +28,13 @@ namespace _0002.MiddleGrammar
                 PrintArray(array[5..]);      // 5번째부터 끝까지
 
                 Range range_5_10 = 5..10;
+                PrintArray(array[range_5_10]); // 5번째부터 9(10-1)번째까지
 
+                Index last = ^0;
+                Range range_5_last = 5..last;
+                PrintArray(array[range_5_last]); // 5번째부터 끝(^)까지
 
+                PrintArray(array[^4..^1]);   // 끝에서 4번째부터 끝(^)에서 2번째까지
             }
         }
     }
