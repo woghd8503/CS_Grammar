18b4a094d233a3bcf5c8c87a3d297eaac380b830
James <woghd8503@naver.com>
2022-09-12 23:57:40 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/050_Queue.cs
+++ This-is-CS/02.MiddleGrammar/050_Queue.cs
@@ -12,6 +12,14 @@ namespace _0002.MiddleGrammar
         static void Main(string[] args)
         {
             Queue que = new Queue();
+            que.Enqueue(1);
+            que.Enqueue(2);
+            que.Enqueue(3);
+            que.Enqueue(4);
+            que.Enqueue(5);
+
+            while(que.Count > 0)
+                Console.WriteLine(que.Dequeue());
         }
     }
 }
