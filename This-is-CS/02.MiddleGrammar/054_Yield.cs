239927fc221ae4af5f6fb2393ff4f3ffc731657b
James <woghd8503@naver.com>
2022-09-13 22:37:27 +0900

    MiddleGrammar

---
+++ This-is-CS/02.MiddleGrammar/054_Yield.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002.MiddleGrammar
+{
+    internal class MyEnumerator
+    {
+        int[] numbers = { 1, 2, 3, 4 };
+        public IEnumerator GetEnumerator()
+        {
+            yield return numbers[0];
+            yield return numbers[1];
+            yield return numbers[2];
+            yield break;
+            yield return numbers[3];
+        }
+    }
+}
