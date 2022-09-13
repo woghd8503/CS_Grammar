afafb37d1cf07987e4b2d0f461cc0090257074b5
James <woghd8503@naver.com>
2022-09-13 22:26:22 +0900

    MiddleGrammar

---
+++ This-is-CS/02.MiddleGrammar/053_Indexer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002.MiddleGrammar
+{
+    internal class _053_Indexer
+    {
+        private int[] array;
+
+        public MyList()
+        {
+            array = new int[3];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+
+            set
+            {
+                if(index >= array.Length)
+                {
+                    Array.Resize<int>(ref array, index + 1);
+                    Console.WriteLine($);
+                }
+            }
+        }
+    }
+}
