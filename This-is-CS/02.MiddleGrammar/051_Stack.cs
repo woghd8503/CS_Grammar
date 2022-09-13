d0644aef09d1ff74e9c2bfdb3f5afea9222c16dd
James <woghd8503@naver.com>
2022-09-13 22:11:18 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/051_Stack.cs
+++ This-is-CS/02.MiddleGrammar/051_Stack.cs
@@ -12,9 +12,13 @@ namespace _0002.MiddleGrammar
         {
             Stack stack = new Stack();
             stack.Push(1);
-            stack.Push(1);
-            stack.Push(1);
-            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            while (stack.Count > 0)
+                Console.WriteLine(stack.Pop());
         }
     }
 }
