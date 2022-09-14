9bde056ad31a958184b7eef3c2b861fca1e24fca
James <woghd8503@naver.com>
2022-09-14 23:56:42 +0900

    MiddleGrammar

--- This-is-CS/02.MiddleGrammar/056_Test.cs
+++ This-is-CS/02.MiddleGrammar/056_Test.cs
@@ -9,6 +9,6 @@ namespace _0002.MiddleGrammar
     internal class _056_Test
     {
         // 다음 배열 선언 문장 중 올바르지 않은 것을 고르세요.
-        // 1. int[] array = new string[3]{ "}
+        // 1. int[] array = new string[3]{ "안녕", "Hello", "Halo" };
     }
 }
