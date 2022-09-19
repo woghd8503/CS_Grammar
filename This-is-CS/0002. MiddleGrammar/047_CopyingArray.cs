c4750ebe794f6a89f3be935a91973d2e4239df55
James <woghd8503@naver.com>
2022-09-20 00:26:35 +0900

    MiddleGrammar

---
+++ This-is-CS/0002. MiddleGrammar/047_CopyingArray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002._MiddleGrammar
+{
+    internal class _047_CopyingArray
+    {
+        static void CopyArray<T>(T[] source, T[] target)
+        {
+            for (int i = 0; i < source.Length; i++)
+                target[i] = source[i];
+        }
+    }
+}
