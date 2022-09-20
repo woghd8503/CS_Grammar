4bb899abd292307b15c40444f6128e7a60cba816
James <woghd8503@naver.com>
2022-09-20 22:51:52 +0900

    MiddleGrammar

---
+++ This-is-CS/0002. MiddleGrammar/059_ConstaraintsOnTypeParameters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002._MiddleGrammar
+{
+    class StructArray<T> where T : struct
+    {
+        public T[] Array { get; set; }
+        public StructArray(int size)
+        {
+            Array = new T[size];
+        }
+    }
+
+    class RefArray<T> where T : class
+    {
+        public T[] Array { get; set; }
+        public RefArray(int size)
+        {
+            Array = new T[size];
+        }
+    }
+
+    class Base { }
+    class Derived : Base { }
+    class BaseArray<U> where U : Base
+    {
+        public U[] Array { get; set; }
+        public BaseArray(int size)
+        {
+            Array = new U[size];
+        }
+
+        public void CopyArray<T>(T[] Source) where T : U
+        {
+            Source.CopyTo(Array, 0);
+        }
+    }
+
+
+}
