955f6b72488fb38ea0dd7e48a44043af8d8ce6a0
James <woghd8503@naver.com>
2022-09-20 22:56:54 +0900

    MiddleGrammar

--- This-is-CS/0002. MiddleGrammar/059_ConstaraintsOnTypeParameters.cs
+++ This-is-CS/0002. MiddleGrammar/059_ConstaraintsOnTypeParameters.cs
@@ -40,5 +40,29 @@ namespace _0002._MiddleGrammar
         }
     }
 
+    class MainApp
+    {
+        public static T CreateInstance<T>() where T : new()
+        {
+            return new T();
+        }
 
+        static void Main(string[] args)
+        {
+            StructArray<int> a = new StructArray<int>(3);
+            a.Array[0] = 0;
+            a.Array[1] = 0;
+            a.Array[2] = 0;
+
+            RefArray<StructArray<double>> b = new RefArray<StructArray<double>>(3);
+            b.Array[0] = new StructArray<double>(5);
+            b.Array[1] = new StructArray<double>(10);
+            b.Array[2] = new StructArray<double>(1005);
+
+            BaseArray<Base> c = new BaseArray<Base>(3);
+            c.Array[0] = new Base();
+            c.Array[1] = new Derived();
+            c.Array[2] = new CreateInstance<Base>();
+        }
+    }
 }
