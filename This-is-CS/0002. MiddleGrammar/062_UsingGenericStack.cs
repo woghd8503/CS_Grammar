842c7d8a2ff374b874046426341c024c012a49b2
James <woghd8503@naver.com>
2022-09-27 23:53:48 +0900

    MiddleGrammar

--- This-is-CS/0002. MiddleGrammar/062_UsingGenericStack.cs
+++ This-is-CS/0002. MiddleGrammar/062_UsingGenericStack.cs
@@ -8,5 +8,18 @@ namespace _0002._MiddleGrammar
 {
     internal class UsingGenericStack
     {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            while (stack.Count > 0)
+                Console.WriteLine(stack.Pop()); ;
+        }
     }
 }
