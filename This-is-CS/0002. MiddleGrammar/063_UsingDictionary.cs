fd7541002bd22f2a916ccee2676fd5d33775378d
James <woghd8503@naver.com>
2022-09-28 22:55:17 +0900

    MiddleGrammar

---
+++ This-is-CS/0002. MiddleGrammar/063_UsingDictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002._MiddleGrammar
+{
+    internal class _063_UsingDictionary
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+        }
+    }
+}
