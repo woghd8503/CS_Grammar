a507e0c8b38088161be7b2a6ae93494f2a6ccc3a
James <woghd8503@naver.com>
2022-09-29 23:09:15 +0900

    MiddleGrmmar

--- This-is-CS/0002. MiddleGrammar/063_UsingDictionary.cs
+++ This-is-CS/0002. MiddleGrammar/063_UsingDictionary.cs
@@ -14,6 +14,14 @@ namespace _0002._MiddleGrammar
 
             dic["하나"] = "one";
             dic["둘"] = "two";
+            dic["셋"] = "three";
+            dic["넷"] = "four";
+            dic["다섯"] = "five";
+
+            Console.WriteLine(dic["하나"]);
+            Console.WriteLine(dic["둘"]);
+            Console.WriteLine(dic["셋"]);
+            Console.WriteLine(dic["다섯"]);
         }
     }
 }
