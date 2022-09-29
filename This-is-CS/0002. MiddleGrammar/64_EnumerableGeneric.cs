e23e1f3fb10b01e96be2b73a2c188693a0bc8414
James <woghd8503@naver.com>
2022-09-29 23:15:50 +0900

    MiddleGrammar

---
+++ This-is-CS/0002. MiddleGrammar/64_EnumerableGeneric.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002._MiddleGrammar
+{
+    internal class MyList<T> : IEnumerable<T>, IEnumerator<T>
+    {
+        private T[] array;
+        int position = -1;
+
+        public MyList()
+        {
+            array = new T[3];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+
+            set
+            {
+                if(index >= array.Length)
+                {
+                    Array.Resize<T>(ref array, index + 1);
+                    Console.WriteLine($"Array Resized : {array.Length}");
+                }
+
+                array[index] = value;
+            }
+        }
+    }
+}
