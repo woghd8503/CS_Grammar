b985dfaa342a3d4b4d00ed3bac1320bed0f217b9
James <woghd8503@naver.com>
2022-10-04 23:41:38 +0900

    MiddleGrammar

---
+++ This-is-CS/02. MiddleGrammar/067_TryCatch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002._MiddleGrammar
+{
+    internal class _067_TryCatch
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+
+            try
+            {
+                for (int i = 0; i< 5; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            catch( IndexOutOfRangeException e )
+            {
+                Console.WriteLine($"예외가 발생했습니다 : {e.Message}");
+            }
+
+            Console.WriteLine("종료");
+        }
+    }
+}
