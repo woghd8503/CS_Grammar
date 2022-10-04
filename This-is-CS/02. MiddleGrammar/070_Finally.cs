05eea94ef9d4e90b54079d7c011d9edc66144b2d
James <woghd8503@naver.com>
2022-10-04 23:56:08 +0900

    MiddleGrammar

---
+++ This-is-CS/02. MiddleGrammar/070_Finally.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002._MiddleGrammar
+{
+    internal class _070_Finally
+    {
+        static int Divide(int dividend, int divisor)
+        {
+            try
+            {
+                Console.WriteLine("Divide() 시작");
+                return dividend / divisor;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Divide() 예외 발생");
+                throw e;
+            }
+            finally
+            {
+                Console.WriteLine("Divide() 끝");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("제수를 입력하세요. :");
+                string temp = Console.ReadLine();
+                int dividend = Convert.ToInt32(temp);
+
+                Console.Write("피제수를 입력하세요. : ");
+                temp = Console.ReadLine();
+                int divisor = Convert.ToInt32(temp);
+
+                Console.WriteLine("{0}/{1} = {2}",
+                    dividend, divisor, Divide(dividend, divisor));
+            
+
+            }
+        }
+    }
+}
