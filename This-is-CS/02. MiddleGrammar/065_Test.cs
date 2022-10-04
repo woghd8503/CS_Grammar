6f709273547558c4d61bc774084df75101ffc667
James <woghd8503@naver.com>
2022-10-04 23:18:04 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/065_Test.cs
+++ This-is-CS/02. MiddleGrammar/065_Test.cs
@@ -9,8 +9,21 @@ namespace _0002._MiddleGrammar
 {
     internal class _065_Test
     {
-        Queue queue = new Queue();
-        queue.Enqueue(10);
+        void Main(string[] args)
+        {
+            // 다음 코드에서 문제를 찾고, 그 원인을 설명하세요.
+            Queue queue = new Queue();
+            queue.Enqueue(10);
+            queue.Enqueue("한글");
+            queue.Enqueue(3.14);
+
+            Queue<int> queue2 = new Queue<int>();
+            queue2.Enqueue(10);
+            //queue2.Enqueue("한글");
+            //queue2.Enqueue(3.14);
+
+            
+        }  
 
 
 
