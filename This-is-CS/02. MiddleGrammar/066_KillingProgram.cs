43d5d2e269aa80bf528586ca87daf56110b53eb4
James <woghd8503@naver.com>
2022-10-04 23:38:10 +0900

    MiddleGrammar

---
+++ This-is-CS/02. MiddleGrammar/066_KillingProgram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002._MiddleGrammar
+{
+    internal class _066_KillingProgram
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine("종료");
+        }
+    }
+}
