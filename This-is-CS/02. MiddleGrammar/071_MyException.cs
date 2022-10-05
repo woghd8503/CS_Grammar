797e7aff1b51a02f7c97b43eec4e756961a71ed1
James <woghd8503@naver.com>
2022-10-05 23:58:31 +0900

    MiddleGrammar

---
+++ This-is-CS/02. MiddleGrammar/071_MyException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002._MiddleGrammar
+{
+    internal class _071_MyException
+    {
+        public InvalidArgumentEception()
+        {
+
+        }
+    }
+}
