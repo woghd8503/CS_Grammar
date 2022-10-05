4794002ea3bc375cba9d4e1dc458d03afdc72a99
James <woghd8503@naver.com>
2022-10-05 23:59:29 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/071_MyException.cs
+++ This-is-CS/02. MiddleGrammar/071_MyException.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _0002._MiddleGrammar
 {
-    internal class _071_MyException
+    internal class _071_MyException : Exception
     {
         public InvalidArgumentEception()
         {
+        }
+
+        public InvalidEnumArgumentException(string message)
+            :base(message)
+        {
 
         }
+
     }
 }
