b9a7b6e257871149caf45d764d58588e87eea27a
James <woghd8503@naver.com>
2022-10-09 13:28:04 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/071_MyException.cs
+++ This-is-CS/02. MiddleGrammar/071_MyException.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Formats.Asn1;
+using System.Security.Cryptography;
 
 namespace _0002._MiddleGrammar
 {
@@ -28,7 +30,33 @@ namespace _0002._MiddleGrammar
 
     class MainApp
     {
-        static unit MergeARGB(unit alpha, unit red, unit )
+        static unit MergeARGB(unit alpha, unit red, unit blue)
+        {
+            unit[] args = new unit[] { alpha, red, green, blue };
+
+            foreach (unit arg in args)
+            {
+                if (arg > 255)
+                    throw new InvalidArgumentException()
+                    {
+                        ArgumentException = arg,
+                        Range = "0~255"
+                    };
+            }
+
+            return (alpha << 24 & 0xFF000000) |
+                (red << 16 & 0x00FF0000) |
+                (green << 8 & 0x0000FF00) |
+                (blue & 0x000000FF);
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("0x{0:X}", MergeARGB(255, 111, 111, 111));
+            }
+        }
 
 
     }
