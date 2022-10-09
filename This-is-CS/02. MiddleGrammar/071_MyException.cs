8204843a7b2e6f622d58d01e50a95616c2cb90a8
James <woghd8503@naver.com>
2022-10-09 13:34:29 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/071_MyException.cs
+++ This-is-CS/02. MiddleGrammar/071_MyException.cs
@@ -1,16 +1,15 @@
 ﻿using System;
-using System.Formats.Asn1;
-using System.Security.Cryptography;
 
-namespace _0002._MiddleGrammar
+
+namespace MyException
 {
-    internal class _071_MyException : Exception
+    class InvalidArgumentException : Exception
     {
-        public InvalidArgumentEception()
+        public InvalidArgumentException()
         {
         }
 
-        public InvalidEnumArgumentException(string message)
+        public InvalidArgumentException(string message)
             :base(message)
         {
         }
@@ -30,24 +29,24 @@ namespace _0002._MiddleGrammar
 
     class MainApp
     {
-        static unit MergeARGB(unit alpha, unit red, unit blue)
+        static uint MergeARGB(uint alpha, uint red, uint green, uint blue)
         {
-            unit[] args = new unit[] { alpha, red, green, blue };
+            uint[] args = new uint[] { alpha, red, green, blue };
 
-            foreach (unit arg in args)
+            foreach (uint arg in args)
             {
                 if (arg > 255)
                     throw new InvalidArgumentException()
                     {
-                        ArgumentException = arg,
+                        Argument = arg,
                         Range = "0~255"
                     };
             }
 
             return (alpha << 24 & 0xFF000000) |
-                (red << 16 & 0x00FF0000) |
-                (green << 8 & 0x0000FF00) |
-                (blue & 0x000000FF);
+                   (red   << 16 & 0x00FF0000) |
+                   (green << 8  & 0x0000FF00) |
+                   (blue        & 0x000000FF);
         }
 
         static void Main(string[] args)
@@ -55,6 +54,8 @@ namespace _0002._MiddleGrammar
             try
             {
                 Console.WriteLine("0x{0:X}", MergeARGB(255, 111, 111, 111));
+                Console.WriteLine("0x{0:X}", MergeARGB(1, 65, 192, 128));
+                Console.WriteLine("0x{0:X}", MergeARGB(0, 255, 255, 300));
             }
         }
 
