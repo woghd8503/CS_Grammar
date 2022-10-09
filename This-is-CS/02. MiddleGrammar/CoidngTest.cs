fde067810a33f047fd851b3323c9178ada42501a
James <woghd8503@naver.com>
2022-10-09 20:45:24 +0900

    MiddleGrammar

---
+++ This-is-CS/02. MiddleGrammar/CoidngTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002._MiddleGrammar
+{
+    internal class CoidngTest
+    {
+        static int IFN = 9999999;
+        
+    }
+}
