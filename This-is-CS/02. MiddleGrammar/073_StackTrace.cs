9b8eac3eec1ed17099e101ace1b827826a1fb5c3
James <woghd8503@naver.com>
2022-10-10 09:15:41 +0900

    MiddleGrammar

--- This-is-CS/02. MiddleGrammar/073_StackTrace.cs
+++ This-is-CS/02. MiddleGrammar/073_StackTrace.cs
@@ -8,5 +8,17 @@ namespace _0002._MiddleGrammar
 {
     internal class _073_StackTrace
     {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 1;
+                Console.WriteLine(3 / --a);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
     }
 }
