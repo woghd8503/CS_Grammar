2572600f6e862953008c48695a653e344ec9c89d
James <woghd8503@naver.com>
2022-10-10 09:21:43 +0900

    MiddleGrammar

---
+++ This-is-CS/02. MiddleGrammar/074_Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002._MiddleGrammar
+{
+    internal class _074_Test
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+
+            for (int i = 0; i < 10; i++)
+                arr[i] = i;
+
+            for (int i = 0; i < 11; i++)
+            {
+                try
+                {
+                    Console.WriteLine(arr[i]);
+
+                }
+                catch(IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("예외처리" + e.Message);
+                }
+            }
+        }
+    }
+}
