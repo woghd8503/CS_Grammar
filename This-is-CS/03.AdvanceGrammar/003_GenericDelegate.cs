08f0adc807c6daf502b4ae349c14cae9ec1b46aa
James <woghd8503@naver.com>
2022-10-10 23:33:40 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/003_GenericDelegate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    delegate int Compare<T>(T a, T b);
+
+    class MainApp
+    {
+        static int AscendCompare<T>(T a, T b) where T : IComparable<T>
+        {
+            return a.CompareTo(b);
+        }
+    }
+    }
+}
