2790c9bf3d1059fd20437af23ef0d7be5f8cfa76
James <woghd8503@naver.com>
2022-10-11 23:57:17 +0900

    AdvanceGrammar

--- This-is-CS/03.AdvanceGrammar/004_DelegateChains.cs
+++ This-is-CS/03.AdvanceGrammar/004_DelegateChains.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace _0003._AdvanceGrammar
 {
+    delegate void Notify(string message);  // Nofity 대리자 선언
+    
+    class Notifier  // Notify 대리자의 인스턴스 EventOccured를 가지느 클래스 Notifier 선언
+    {
+        public Notify EventOccured;
+    }
+
     internal class _004_DelegateChains
     {
     }
