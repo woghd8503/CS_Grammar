7d6f22521893a09bab3ae188ddb439706b9b2ec6
James <woghd8503@naver.com>
2022-10-16 18:13:16 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/005_AnonymousMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    delegate int Compare(int a, int b);
+    class MainApp
+    {
+
+    }
+}
