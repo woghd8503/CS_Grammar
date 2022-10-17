e576b88943438dac43cc7b86e5131bf14b962e83
James <woghd8503@naver.com>
2022-10-17 20:56:26 +0900

    AdvanceGrammar

--- This-is-CS/03.AdvanceGrammar/005_AnonymousMethod.cs
+++ This-is-CS/03.AdvanceGrammar/005_AnonymousMethod.cs
@@ -15,9 +15,9 @@ namespace _0003._AdvanceGrammar
             int j = 0;
             int temp = 0;
 
-            for (i = 0; i < DataSet.Length -1; i++)
+            for (i = 0; i < DataSet.Length - 1; i++)
             {
-                for (j = 0; j < DataSet.Length -1; j++)
+                for (j = 0; j < DataSet.Length - 1; j++)
                 {
                     if (Comparer(DataSet[j], DataSet[j + 1]) > 0)
                     {
@@ -45,8 +45,23 @@ namespace _0003._AdvanceGrammar
                 });
             for (int i = 0; i < array.Length; i++)
                 Console.Write($"{array[i]} ");
+
+            int[] array2 = { 7, 2, 8, 10, 11 };
+            Console.WriteLine("\nSorting descending...");
+            BubbleSort(array2, delegate (int a, int b) // 익명 메소드
+                {
+                    if (a < b)
+                        return 1;
+                    else if (a == b)
+                        return 0;
+                    else
+                        return -1;
+                });
+
+            for (int i = 0; i < array2.Length; i++)
+                Console.Write($"{array2[i]} ");
+
+            Console.WriteLine();
         }
-
-
     }
 }
