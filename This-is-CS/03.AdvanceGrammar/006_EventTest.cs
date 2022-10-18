413bd32d26fbce1717427e152d5b5062a378b291
James <woghd8503@naver.com>
2022-10-18 20:57:00 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/006_EventTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+
+    delegate void EventHandler(string message);
+
+    class MyNotifier
+    {
+        public event EventHandler SomethingHappened;
+        public void DoSomthing(int number)
+        {
+            int temp = number % 10;
+
+            if (temp != 0 && temp % 3 == 0)
+            {
+                SomethingHappened(String.Format("{0} : 짝", number));
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static public void MyHandler(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static void Main(string[] args)
+        {
+            MyNotifier notifier = new MyNotifier();
+            notifier.SomethingHappened += new EventHandler(MyHandler);
+
+            for
+        }
+    }
+}
