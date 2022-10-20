1eac48772977264cf56db1fe6cc203cda0b2b532
James <woghd8503@naver.com>
2022-10-20 22:41:40 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/008_SimpleLambda.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    internal class _008_SimpleLambda
+    {
+        delegate int Calculate(int a, int b);
+
+
+        static void Main(string[] args)
+        {
+            Calculate calc = (a, b) => a + b;
+
+            Console.WriteLine($"{3} + {4} : {calc(3, 4)}");
+        }
+    }
+}
