5823181217b1c4abc897a79f6946890ec1fe4848
James <woghd8503@naver.com>
2022-10-22 11:00:00 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/011_ActionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Action act1 = () => Console.WriteLine("Action()");
+            act1();
+
+            int result = 0;
+            Action<int> act2 = (x) => result = x * x;
+
+            act2(3);
+            Console.WriteLine($"result : {result}");
+
+            Action<double, double> act3 = (x, y) =>
+            {
+                double pi = x / y;
+                Console.WriteLine($"Action<T1, T2>({x}, {y}) : {pi}");
+            };
+        }
+    }
+}
