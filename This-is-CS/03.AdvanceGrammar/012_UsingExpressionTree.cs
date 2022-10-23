72392b169a6e2b8c5424c6f985f49e5b967ac7ff
James <woghd8503@naver.com>
2022-10-23 12:20:41 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/012_UsingExpressionTree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            // 1*2(x-y)
+            Expression const1 = Expression.Constant(1);
+            Expression const2 = Expression.Constant(2);
+
+            Expression leftExp = Expression.Multiply(const1, const2); // 1 * 2
+
+
+        }
+    }
+}
