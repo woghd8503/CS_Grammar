64d3beac190fa878899ec6ddca851bb1dabaaf85
James <woghd8503@naver.com>
2022-10-25 23:51:14 +0900

    AdvanceGrammar

--- This-is-CS/03.AdvanceGrammar/013_ExpressionTreeViaLambda.cs
+++ This-is-CS/03.AdvanceGrammar/013_ExpressionTreeViaLambda.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,14 @@ namespace _0003._AdvanceGrammar
 {
     internal class _013_ExpressionTreeViaLambda
     {
+        static void Main(string[] args)
+        {
+            Expression<Func<int, int, int>> expression =
+                (a, b) => 1 * 2 + (a - b);
+            Func<int, int, int> func = expression.Compile();
+
+            // x = 7, y = 8
+            Console.WriteLine($"1*2+{})
+        }
     }
 }
