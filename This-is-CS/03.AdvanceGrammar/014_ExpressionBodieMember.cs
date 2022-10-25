7795e5ec385df0e3d855c11eee2f832198b2bb85
James <woghd8503@naver.com>
2022-10-26 00:04:28 +0900

    AdvanceGrammar

--- This-is-CS/03.AdvanceGrammar/014_ExpressionBodieMember.cs
+++ This-is-CS/03.AdvanceGrammar/014_ExpressionBodieMember.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace _0003._AdvanceGrammar
 {
-    class _014_ExpressionBodieMember
+    class FriendList
     {
         private List<string> list = new List<string>();
 
         public void Add(string name) => list.Add(name);
-        public void Remove(string name) => list
+        public void Remove(string name) => list.Remove(name);
+        public void PrintAll()
+        {
+            foreach (var s in list)
+                Console.WriteLine(s);
+        }
+
+        public FriendList() => Console.WriteLine("FriendList()");
+        ~FriendList() => Console.WriteLine("~FriendList()");
     }
 }
