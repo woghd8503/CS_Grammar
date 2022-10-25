c6e2c1263be6179748af70bf8023c321927a2051
James <woghd8503@naver.com>
2022-10-25 23:54:48 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/014_ExpressionBodieMember.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    class _014_ExpressionBodieMember
+    {
+        private List<string> list = new List<string>();
+
+        public void Add(string name) => list.Add(name);
+        public void Remove(string name) => list
+    }
+}
