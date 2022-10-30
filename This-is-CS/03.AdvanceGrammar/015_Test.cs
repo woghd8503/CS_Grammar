c7d1e2e78108680256385b3b26d5040c7d71d286
James <woghd8503@naver.com>
2022-10-31 02:17:32 +0900

    AdvanceGrammar

--- This-is-CS/03.AdvanceGrammar/015_Test.cs
+++ This-is-CS/03.AdvanceGrammar/015_Test.cs
@@ -18,6 +18,23 @@ namespace _0003._AdvanceGrammar
             Console.WriteLine(func_1() + func_2(30));
 
             // 다음 코드에서 익명 메소드를 람다식으로 수정하세요.
+            int[] array = { 11, 22, 33, 44, 55 };
+
+            foreach (int a in array)
+            {
+                Action action = new Action
+                    (
+                        delegate ()
+                        {
+                            Console.WriteLine(a * a);
+                        }
+                    );
+                action.Invoke();
+
+                // 정답
+                Action<int> action1 = (x) => { Console.WriteLine(x * x); };
+                action1(a);
+            }
         }
     }
 }
