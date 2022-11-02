0c50cba9ce6600fa560555ba9ae5bda43b5b8fcb
James <woghd8503@naver.com>
2022-11-03 00:48:16 +0900

    AdvanceGrammar

--- This-is-CS/03.AdvanceGrammar/017_SimpleLinq.cs
+++ This-is-CS/03.AdvanceGrammar/017_SimpleLinq.cs
@@ -19,7 +19,21 @@ namespace _0003._AdvanceGrammar
             {
                 new Profile(){Name="정우성", Height=186},
                 new Profile(){Name="김태희", Height=158},
-            }
+                new Profile(){Name="고현정", Height=172},
+                new Profile(){Name="이문세", Height=178},
+                new Profile(){Name="하하", Height=171},
+            };
+
+            var profiles = from profile in arrProfile
+                          where profile.Height < 175
+                          orderby profile.Height
+                          select new
+                          {
+                              Name = profile.Name,
+                              InchHeight = profile.Height * 0.393
+                          };
+            foreach (var profile in profiles)
+                Console.WriteLine($"{profile.Name}, {profile.InchHeight}");
         }
     }
 }
