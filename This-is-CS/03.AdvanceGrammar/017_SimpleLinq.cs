7bf7f4fc5680db0fee0de0924274e367a23d8dcc
James <woghd8503@naver.com>
2022-11-03 00:39:23 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/017_SimpleLinq.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    class Profile
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+    }
+    internal class _017_SimpleLinq
+    {
+        static void Main(string[] args)
+        {
+            Profile[] arrProfile =
+            {
+                new Profile(){Name="정우성", Height=186},
+                new Profile(){Name="김태희", Height=158},
+            }
+        }
+    }
+}
