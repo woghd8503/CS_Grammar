238bde20ac35e66124ad8d66d218c8d574d84622
James <woghd8503@naver.com>
2022-11-04 01:00:12 +0900

    AdvanceGrammar

--- This-is-CS/03.AdvanceGrammar/018_FromFrom.cs
+++ This-is-CS/03.AdvanceGrammar/018_FromFrom.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace _0003._AdvanceGrammar
 {
     class Class
-    { 
+    {
         public string Name { get; set; }
         public int[] Score { get; set; }
     }
@@ -18,12 +18,19 @@ namespace _0003._AdvanceGrammar
         {
             Class[] arrClass =
                 {
-                new Class
+                new Class(){Name="연두반", Score=new int[]{ 99, 70, 24}},
+                new Class(){Name="분홍반", Score=new int[]{ 60, 45, 87, 72}},
+                new Class(){Name="파랑반", Score=new int[]{ 92, 30, 85, 94}},
+                new Class(){Name="노랑반", Score=new int[]{ 90, 88, 0, 17}}
             };
-        }
-    }
-    internal class _018_FromFrom
-    {
+            var classes = from c in arrClass
+                          from s in c.Score
+                          where s < 60
+                          orderby s
+                          select new { c.Name, Lowest = s };
 
+            foreach (var c in classes)
+                Console.WriteLine($"낙제 : {c.Name} ({c.Lowest})");
+        }
     }
 }
