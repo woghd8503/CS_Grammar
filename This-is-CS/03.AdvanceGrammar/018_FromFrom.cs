3cee91fdfb379896bfc1233d8d0c780c59828e63
James <woghd8503@naver.com>
2022-11-03 21:50:26 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/018_FromFrom.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    class Class
+    { 
+        public string Name { get; set; }
+        public int[] Score { get; set; }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Class[] arrClass =
+                {
+                new Class
+            };
+        }
+    }
+    internal class _018_FromFrom
+    {
+
+    }
+}
