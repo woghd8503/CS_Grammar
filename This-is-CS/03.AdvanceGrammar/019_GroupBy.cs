e96a18e1981b6f1e397aabb36e20e7832b0f41d5
James <woghd8503@naver.com>
2022-11-05 23:52:55 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/019_GroupBy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    class Profile
+    {
+        public string name { get; set; }
+        public int Height { get; set; }
+    }
+    internal class _019_GroupBy
+    {
+        static void Main(string[] args)
+        {
+            new Profile() { }
+        }
+    }
+}
