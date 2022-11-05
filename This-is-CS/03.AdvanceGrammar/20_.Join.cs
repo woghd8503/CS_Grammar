d8dd759c6064012d75063541b2f0d7cd8f68f649
James <woghd8503@naver.com>
2022-11-06 01:16:05 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/20_.Join.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    class Profile
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+    }
+
+    class Product
+    {
+        public string Tile { get; set; }
+        public string Start { get; set; }
+    }
+}
