3d3033ca4e3c14f78f89d2bf055203875ae95475
James <woghd8503@naver.com>
2022-11-06 00:56:50 +0900

    AdvanceGrammar

--- This-is-CS/03.AdvanceGrammar/019_GroupBy.cs
+++ This-is-CS/03.AdvanceGrammar/019_GroupBy.cs
@@ -19,7 +19,12 @@ namespace _0003._AdvanceGrammar
             {
             new Profile() {name="정우성", Height=186},
             new Profile() {name="김태희", Height=158},
-            }
+            new Profile() {name="이문세", Height=178},
+            new Profile() {name="하하", Height=171},
+            };
+
+            var listProfile = from pofile in arrProfile
+                              orderby pro
         }
     }
 }
