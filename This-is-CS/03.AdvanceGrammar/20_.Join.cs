278bbe88913c4274200b068d74d608be4dc6ae05
James <woghd8503@naver.com>
2022-11-06 03:54:24 +0900

    AdvanceGrammar

--- This-is-CS/03.AdvanceGrammar/20_.Join.cs
+++ This-is-CS/03.AdvanceGrammar/20_.Join.cs
@@ -17,4 +17,17 @@ namespace _0003._AdvanceGrammar
         public string Tile { get; set; }
         public string Start { get; set; }
     }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Profile[] arrProfile =
+            {
+                new Profile(){ Name="정우성", Height=186},
+                new Profile(){ Name="김태희", Height=158},
+                new Profile(){ }
+            }
+        }
+    }
 }
