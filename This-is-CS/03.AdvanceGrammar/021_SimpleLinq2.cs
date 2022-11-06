6c969acfcb3f55f211e75b23efb0d33e63616407
James <woghd8503@naver.com>
2022-11-07 03:25:46 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/021_SimpleLinq2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    class Profile
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+    }
+    internal class _021_SimpleLinq2
+    {
+    }
+}
