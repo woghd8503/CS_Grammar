d3ba47a79877b04d2e81847733e1fd125243d3fb
James <woghd8503@naver.com>
2022-11-06 15:04:03 +0900

    AdvanceGrammar

--- This-is-CS/03.AdvanceGrammar/20_.Join.cs
+++ This-is-CS/03.AdvanceGrammar/20_.Join.cs
@@ -24,10 +24,29 @@ namespace _0003._AdvanceGrammar
         {
             Profile[] arrProfile =
             {
-                new Profile(){ Name="정우성", Height=186},
-                new Profile(){ Name="김태희", Height=158},
-                new Profile(){ }
-            }
+                new Profile(){Name="정우성", Height=186},
+                new Profile(){Name="김태희", Height=158},
+                new Profile(){Name="고현정", Height=172},
+                new Profile(){Name="이문세", Height=178},
+                new Profile(){Name="하하", Height=171}
+            };
+
+            Product[] arrProduct =
+            {
+                new Product(){Title="비트",          Star="정우성"},
+                new Product(){Title="CF 다수",       Star="김태희"},
+                new Product(){Title="아이리스",       Star="김태희"},
+                new Product(){Title="모래시계",       Star="고현정"},
+                new Product(){Title="Solo 예찬",       Star="이문세"},
+            };
+
+            var listProfile =
+                from pofile in arrProfile
+                join product in 
+
+            var listProfile =
+                from profile in arrProfile
+                join product in arrProduct on 
         }
     }
 }
