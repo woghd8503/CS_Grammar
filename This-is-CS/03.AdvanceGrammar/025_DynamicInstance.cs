4c0333dc606f99b90cb68eecbd4aff630d955bb6
James <woghd8503@naver.com>
2022-11-27 08:20:42 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/025_DynamicInstance.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    class Profile
+    {
+        private string name;
+        private string phone;
+        public Profile()
+        {
+            name = ""; phone = "";
+        }
+
+        public Profile(string name, string phone)
+        {
+            this.name = name;
+            this.phone = phone;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{name}, {phone}");
+        }
+
+        public string Name
+        {
+            get
+        }
+    }
+}
