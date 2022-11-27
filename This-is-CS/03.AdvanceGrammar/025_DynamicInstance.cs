074bc568fea41e0fc414188d3e1769f46560987c
James <woghd8503@naver.com>
2022-11-27 15:02:35 +0900

    AdvanceGrammar

--- This-is-CS/03.AdvanceGrammar/025_DynamicInstance.cs
+++ This-is-CS/03.AdvanceGrammar/025_DynamicInstance.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,7 +29,37 @@ namespace _0003._AdvanceGrammar
 
         public string Name
         {
-            get
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Type type = Type.GetType("DynamicInstance.Profile");
+            MethodInfo methodInfo = type.GetMethod("Print");
+
+            PropertyInfo nameProperty = type.GetProperty("Name");
+            PropertyInfo phoneProperty = type.GetProperty("Phone");
+
+            object profile = Activator.CreateInstance(type, "박상현", "512-1234");
+            methodInfo.Invoke(profile, null);
+
+            profile = Activator.CreateInstance(type);
+            nameProperty.SetValue(profile, "박찬호", null);
+            phoneProperty.SetValue(profile, "997-5511", null);
+
+            Console.WriteLine("{0}, {1}",
+                nameProperty.GetValue(profile, null),
+                phoneProperty.GetValue(profile, null));
         }
     }
 }
