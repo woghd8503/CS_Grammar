f547fcef0f280d16035c754cdbc1dae02728034a
James <woghd8503@naver.com>
2022-11-29 23:35:44 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/026_EmitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    public class MainApp
+    {
+        public static void Main()
+        {
+            AssemblyBuilder newAssembly =
+                AssemblyBuilder.DefineDynamicAssembly(
+                    new System.Reflection.AssemblyName("CalculatorAssembly"),
+                    AssemblyBuilderAccess.Run);
+
+            ModuleBuilder newModule = newAssembly.DefineDynamicModule(
+                "Calculator");
+            TypeBuilder new
+        }
+    }
+}
