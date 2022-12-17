224dd0867b62da3fc22544d7d9d65995a2702705
James <woghd8503@naver.com>
2022-12-18 01:14:35 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/027_BasicAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    class MyClass
+    {
+        [Obsolete("OldMethod는 폐기되었습니다. NewMethod()를 이용하세요.")]
+        public void OldMethod()
+        {
+            Console.WriteLine("I'm old");
+        }
+    }
+}
