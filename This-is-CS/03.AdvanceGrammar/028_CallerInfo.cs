24e1e1f1fa271e21e92dfa8a16383d72be6ffbaa
James <woghd8503@naver.com>
2022-12-21 11:00:00 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/028_CallerInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    public static class Trace
+    {
+        public static void WriteLin(string message,
+            [CallerFilePath] string file = "",
+            [CallerLineNumber] int line = 0,
+            [CallerFilePath] string member = "")
+        {
+            Console.WriteLine(
+                $"{file}(Line:{line}) {member}: {message}");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Trace.WriteLin("즐거운 프로그래밍!");
+        }
+    }
+}
