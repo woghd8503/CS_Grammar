1f4996242208cc4d42a6338b6c36903c31c2146b
James <woghd8503@naver.com>
2022-12-23 05:12:05 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/029_HistroyAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.BasicGrammar
+{
+    [AttributeUsage(System.AttributeTargets.Class, AllowMultiple = true)]
+    class Histroy : System.Attribute
+    {
+        private string programmer;
+        public double version;
+        public string changes;
+
+        public Histroy(string programmer)
+        {
+            this.programmer = programmer;
+            version = 1.0;
+            changes = "First release";
+        }
+
+        public string GetProgrammer()
+        {
+            return programmer;
+        }
+    }
+
+    [Histroy]
+}
