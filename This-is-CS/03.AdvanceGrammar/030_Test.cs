7e089cd7060017bd49bfed0715f9c213c46a39b5
James <woghd8503@naver.com>
2022-12-24 22:57:31 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/030_Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    // 다음 코드 중에서 올바로 동작하지 않는 것을 고르세요.
+    // 1. Type t = myObject.GetType();
+    // 2. Type t = typeof("int");
+    // 3. Type t = type.GetType(int);
+    // 4. Type t = type.GetType("System.Int32");
+
+    // 2. 애트리뷰트와 주석의 차이는 무엇입니까?
+}
