a9720a32a412ca39b9c263e3dc6bcba47926266f
James <woghd8503@naver.com>
2023-01-08 10:53:24 +0900

    AdvanceGrammar

--- This-is-CS/03.AdvanceGrammar/033_WithPyton.cs
+++ This-is-CS/03.AdvanceGrammar/033_WithPyton.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Scripting.Hosting;
+using Iron
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +10,16 @@ namespace _0003._AdvanceGrammar
 {
     internal class _033_WithPyton
     {
-
+        class MainApp
+        {
+            static void Main(string[] args)
+            {
+                scriptEngine engine = Python.CreateEngine();
+                ScriptScope scope = engine.CreateScope();
+                scope.SetVariable("n", "박상현");
+                scope.SetVariable("p", "010-123-4566");
+            }
+        }
         // 
 
     }
