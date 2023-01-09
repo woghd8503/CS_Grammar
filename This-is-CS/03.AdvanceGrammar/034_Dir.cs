073235d6a08746046df569b545173efdc300a4af
James <woghd8503@naver.com>
2023-01-09 22:12:14 +0900

    AdvanceGrammar

---
+++ This-is-CS/03.AdvanceGrammar/034_Dir.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    internal class _034_Dir
+    {
+        static void Main(string[] args)
+        {
+            string directory;
+            if (args.Length < 1)
+                directory = ".";
+            else
+                directory = args[0];
+
+            Console.WriteLine($"{directory} directory Info");
+            Console.WriteLine("- Directories :");
+            var directories = (from dir in Directory.GetDirectories(directory)
+                               let info = new DirectoryInfo(dir)
+                               select new
+                               {
+                                   Name = info.Name,
+                                   Attributes = info.Attributes
+                               }).ToList();
+
+            foreach(var d in directories)
+                Console.WriteLine($"{d.Name} : {d.Attributes}");
+
+            Console.WriteLine("- Files :");
+            var files = (from file in Dir)
+        }
+
+    }
+}
