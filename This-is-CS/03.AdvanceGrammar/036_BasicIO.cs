83253a44fe2ebc70ee07688a205b014efc7e02be
James <woghd8503@naver.com>
2023-01-29 17:59:11 +0900

    AdvancedGrammar

--- This-is-CS/03.AdvanceGrammar/036_BasicIO.cs
+++ This-is-CS/03.AdvanceGrammar/036_BasicIO.cs
@@ -8,5 +8,10 @@ namespace _0003._AdvanceGrammar
 {
     internal class _036_BasicIO
     {
+        static void Main(string[] args)
+        {
+            long someValue = 0x123456789ABCDEF0;
+            Console.WriteLine("0,-1");
+        }
     }
 }
