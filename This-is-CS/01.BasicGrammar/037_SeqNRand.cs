cf8c338dc1e6eda03dd6ae810719a292173c843d
James <woghd8503@naver.com>
2023-02-12 01:53:30 +0900

    AdvancedGrammar

---
+++ This-is-CS/01.BasicGrammar/037_SeqNRand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _01.BasicGrammar
+{
+    internal class _037_SeqNRand
+    {
+        static void Main(string[] args)
+        {
+            Stream outStream = new FileStream("a.dat", FileMode.Create);
+        }
+    }
+}
