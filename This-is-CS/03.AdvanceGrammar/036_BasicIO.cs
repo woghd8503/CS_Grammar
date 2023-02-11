3a5f2034812236f31e4afbfbf016437b6c267839
James <woghd8503@naver.com>
2023-02-11 22:43:06 +0900

    AdvancedGrammar

--- This-is-CS/03.AdvanceGrammar/036_BasicIO.cs
+++ This-is-CS/03.AdvanceGrammar/036_BasicIO.cs
@@ -19,7 +19,24 @@ namespace _0003._AdvanceGrammar
             Console.Write("{0,-13} : ", "Byte array");
 
             foreach (byte b in wBytes)
-                Console.Write("{0, -13}");
+                Console.Write("{0:X2}", "b");
+            Console.WriteLine();
+
+            outStream.Write(wBytes, 0, wBytes.Length);
+            outStream.Close();
+
+            Stream inStream = new FileStream("a.dat", FileMode.Open);
+            byte[] rbytes = new byte[8];
+
+            int i = 0;
+            while (inStream.Position < inStream.Length)
+                rbytes[i++] = (byte)inStream.ReadByte();
+
+            long readValue = BitConverter.ToInt64(rbytes, 0);
+
+            Console.WriteLine("{0,-13} : 0x{1:X16} ", "Read Data", readValue);
+            inStream.Close();
+            
         }
     }
 }
