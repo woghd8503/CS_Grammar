a392eeba5c1396d0939c2444678985c0440923ef
James <woghd8503@naver.com>
2023-02-13 00:52:57 +0900

    AdvancedGrammar

--- This-is-CS/01.BasicGrammar/037_SeqNRand.cs
+++ This-is-CS/01.BasicGrammar/037_SeqNRand.cs
@@ -12,6 +12,25 @@ namespace _01.BasicGrammar
         static void Main(string[] args)
         {
             Stream outStream = new FileStream("a.dat", FileMode.Create);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x01);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x02);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x03);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.Seek(5, SeekOrigin.Current);
+
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x04);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.Close();
         }
     }
 }
---
+++ This-is-CS/03.AdvanceGrammar/037_SeqNRand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _01.BasicGrammar
+{
+    internal class _037_SeqNRand
+    {
+        static void Main(string[] args)
+        {
+            Stream outStream = new FileStream("a.dat", FileMode.Create);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x01);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x02);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x03);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.Seek(5, SeekOrigin.Current);
+
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x04);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.Close();
+        }
+    }
+}
