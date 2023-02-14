8fe58724b15ddb9c0320433cb0e6433d6b4ad3f2
James <woghd8503@naver.com>
2023-02-14 23:26:13 +0900

    AdvancedGrammar

---
+++ This-is-CS/03.AdvanceGrammar/040_TextFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0003._AdvanceGrammar
+{
+    internal class _040_TextFile
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter sw =
+                new StreamWriter(
+                    new FileStream("a.txt", FileMode.Create)))
+            {
+                sw.WriteLine(int.MaxValue);
+            }
+        }
+    }
+}
