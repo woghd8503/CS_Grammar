2ea1e3423658900622f70810c72865796c204d63
James <woghd8503@naver.com>
2023-03-03 23:43:46 +0900

    AdvancedGrammar

---
+++ This-is-CS/03.AdvanceGrammar/042_SerializingCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace _0003._AdvanceGrammar
+{
+    internal class _042_SerializingCollection
+    {
+        [Serializable]
+    }
+}
