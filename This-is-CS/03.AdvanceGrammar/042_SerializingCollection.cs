f4f578c6271cd17c74c5ee6dac90fe81bea61618
James <woghd8503@naver.com>
2023-03-20 00:44:16 +0900

    AdvancedGrammar

--- This-is-CS/03.AdvanceGrammar/042_SerializingCollection.cs
+++ This-is-CS/03.AdvanceGrammar/042_SerializingCollection.cs
@@ -11,6 +11,39 @@ namespace _0003._AdvanceGrammar
     internal class _042_SerializingCollection
     {
         [Serializable]
+        class NameCard
+        {
+            public NameCard(string Name, string Phone, int Age)
+            {
+                this.Name = Name;
+                this.Phone = Phone;
+                this.Age = Age;
+            }
+
+            public string Name;
+            public string Phone;
+            public int Age;
+        }
+
+        class MainApp
+        {
+            static void Main(string[] agrs)
+            {
+                using (Stream ws = new FileStream("a.dat", FileMode.Create))
+                {
+                    BinaryFormatter serializer = new BinaryFormatter();
+
+                    List<NameCard> list = new List<NameCard>();
+                    list.Add(new NameCard("박상현", "010-123-4567", 33));
+                    list.Add(new NameCard("김연아", "010-323-1111", 22));
+                    list.Add(new NameCard("장미란", "010-555-5555", 26));
+
+                    serializer.Serialize(ws, list);
+                }
+
+                using Stream rs = new FileStream("a.dat", FileMode.Open);
+            }
+        }
 
     }
 }
